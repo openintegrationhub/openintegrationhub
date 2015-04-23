69da0be869bc69a0abbcfb66c08765859ec012e9
ellenaua <elenasharovar@gmail.com>
2015-04-23 15:53:19 +0300

    Connection of a "sailor" to rabbitmq and mongo

--- run.js
+++ run.js
@@ -2,3 +2,14 @@
 // then connect to mongo
 // listen incoming messages
 // and run client component
+
+var init = require('./lib/init.js').init;
+
+init(function(err, connections) {
+    if (err) {
+        // @TODO how do we report errors?
+        return;
+    }
+
+});
+
