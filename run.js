3f07c28158ee143e06fe6d56e4e3f0cf7b41e800
ellenaua <elenasharovar@gmail.com>
2015-05-19 18:30:16 +0300

    Push taskStats to the queue also. Refs #2015

--- run.js
+++ run.js
@@ -1,12 +1,13 @@
+var logging = require('./lib/logging.js');
 var Sailor = require('./lib/sailor.js').Sailor;
 var settings = require('./lib/settings.js').readFrom(process.env);
-var errorReporter = require('./lib/error_reporter.js');
+
 
 sailor = new Sailor(settings);
 
 sailor.connect()
     .then(sailor.run.bind(sailor))
-    .fail(errorReporter.reportError)
+    .fail(logging.criticalError)
     .done();
 
 process.on('SIGTERM', function() {
