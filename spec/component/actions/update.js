8b72bbc9aa9b465c0fea1f5ad6dbcaf451ecb5a7
Pavel Nedelko <pnedelko@gmail.com>
2015-06-03 09:59:08 +0300

    runService entry point

--- spec/component/actions/update.js
+++ spec/component/actions/update.js
@@ -1,4 +1,14 @@
 exports.process = processAction;
+exports.getMetaModel = getMetaModel;
+exports.getModel = getModel;
 
 function processAction(msg, cfg){
+}
+
+function getMetaModel(cfg, cb) {
+    return cb(null, 'metamodel');
+}
+
+function getModel(cfg, cb) {
+    return cb(null, 'model')
 }
