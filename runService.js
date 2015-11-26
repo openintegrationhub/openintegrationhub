e977e21a15cd0ebfcdd842a3a5e1b099d6a2af8b
Pavel Nedelko <pnedelko@gmail.com>
2015-11-26 14:10:56 +0100

    added some debug messages

--- runService.js
+++ runService.js
@@ -1,8 +1,11 @@
 var Q = require('q');
 var logging = require('./lib/logging');
 var service = require('./lib/service');
+var debug = require('debug')('sailor');
 
 var serviceMethod = process.argv[2];
+
+debug('About to execute %s', serviceMethod);
 
 service.processService(serviceMethod, process.env)
     .catch(logging.criticalError)
