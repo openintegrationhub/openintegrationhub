cf78aa246286c37daf2a578388caec9940e4f5a4
Igor Drobiazko <igor@elastic.io>
2016-11-04 08:39:46 +0100

    Initial Promises support

---
+++ spec/component/triggers/promise_request_trigger.js
@@ -0,0 +1,19 @@
+const Q = require('q');
+const request = require('request');
+
+exports.process = processTrigger;
+
+function processTrigger(msg, cfg) {
+    const options = {
+        uri: 'http://promise_target_url:80/foo/bar',
+        json: true
+    };
+
+    return Q.ninvoke(request, 'get', options)
+        .spread((req, body) => body)
+        .then((data) => {
+            return {
+                body: data
+            };
+        });
+}
--- spec/executor.spec.js
+++ spec/executor.spec.js
@@ -1,5 +1,7 @@
 describe('Executor', function () {
 
+    const nock = require('nock');
+
     var TaskExec = require('../lib/executor.js').TaskExec;
     var payload = {content: "MessageContent"};
     var cfg = {};
@@ -11,11 +13,18 @@ describe('Executor', function () {
         spyOn(taskexec, 'emit').andCallThrough();
 
         var module = require('./component/triggers/passthrough.js');
-        taskexec.process(module, payload, cfg);
 
-        expect(taskexec.emit).toHaveBeenCalled();
-        expect(taskexec.emit.calls[0].args[0]).toEqual('data');
-        expect(taskexec.emit.calls[1].args[0]).toEqual('end');
+        runs(() => {
+            taskexec.process(module, payload, cfg);
+        });
+
+        waitsFor(() => taskexec.emit.callCount > 1, 5000);
+
+        runs(() => {
+            expect(taskexec.emit).toHaveBeenCalled();
+            expect(taskexec.emit.calls[0].args[0]).toEqual('data');
+            expect(taskexec.emit.calls[1].args[0]).toEqual('end');
+        });
     });
 
     it('Should reject if module is missing', function () {
@@ -23,12 +32,18 @@ describe('Executor', function () {
         taskexec.on('error', function(){});
         spyOn(taskexec, 'emit').andCallThrough();
 
-        taskexec.process({}, payload, cfg);
+        runs(() => {
+            taskexec.process({}, payload, cfg);
+        });
 
-        expect(taskexec.emit).toHaveBeenCalled();
-        expect(taskexec.emit.calls[0].args[0]).toEqual('error');
-        expect(taskexec.emit.calls[0].args[1].message).toEqual('Process function is not found');
-        expect(taskexec.emit.calls[1].args[0]).toEqual('end');
+        waitsFor(() => taskexec.emit.callCount > 1, 5000);
+
+        runs(() => {
+            expect(taskexec.emit).toHaveBeenCalled();
+            expect(taskexec.emit.calls[0].args[0]).toEqual('error');
+            expect(taskexec.emit.calls[0].args[1].message).toEqual('Process function is not found');
+            expect(taskexec.emit.calls[1].args[0]).toEqual('end');
+        });
     });
 
     it('Should execute rebound_trigger and emit all events - rebound, end', function () {
@@ -37,11 +52,18 @@ describe('Executor', function () {
         spyOn(taskexec, 'emit').andCallThrough();
 
         var module = require('./component/triggers/rebound_trigger.js');
-        taskexec.process(module, payload, cfg);
 
-        expect(taskexec.emit).toHaveBeenCalled();
-        expect(taskexec.emit.calls[0].args[0]).toEqual('rebound');
-        expect(taskexec.emit.calls[1].args[0]).toEqual('end');
+        runs(() => {
+            taskexec.process(module, payload, cfg);
+        });
+
+        waitsFor(() => taskexec.emit.callCount > 1, 5000);
+
+        runs(() => {
+            expect(taskexec.emit).toHaveBeenCalled();
+            expect(taskexec.emit.calls[0].args[0]).toEqual('rebound');
+            expect(taskexec.emit.calls[1].args[0]).toEqual('end');
+        });
     });
 
     it('Should execute complex trigger, and emit all 6 events', function () {
@@ -75,23 +97,94 @@ describe('Executor', function () {
         var module = require('./component/triggers/test_trigger.js');
         taskexec.process(module, payload, cfg);
 
-        expect(taskexec.emit).toHaveBeenCalled();
+        waitsFor(() => taskexec.emit.callCount >= 9, 5000);
 
-        var calls = taskexec.emit.calls;
+        runs(function(){
 
-        expect(calls[0].args).toEqual(['data', 'Data 1']);
-        expect(calls[1].args).toEqual(['data', {content:'Data 2'}]);
-        expect(calls[2].args).toEqual(['data']);
+            expect(taskexec.emit).toHaveBeenCalled();
 
-        expect(calls[3].args).toEqual(['error', 'Error 1']);
-        expect(calls[4].args).toEqual(['error', {}]);
-        expect(calls[5].args).toEqual(['error']);
+            var calls = taskexec.emit.calls;
 
-        expect(calls[6].args).toEqual(['rebound', 'Rebound Error 1']);
-        expect(calls[7].args).toEqual(['rebound', {}]);
-        expect(calls[8].args).toEqual(['rebound']);
+            expect(calls[0].args).toEqual(['data', 'Data 1']);
+            expect(calls[1].args).toEqual(['data', {content:'Data 2'}]);
+            expect(calls[2].args).toEqual(['data']);
 
-        expect(calls[9].args[0]).toEqual('end');
+            expect(calls[3].args).toEqual(['error', 'Error 1']);
+            expect(calls[4].args).toEqual(['error', {}]);
+            expect(calls[5].args).toEqual(['error']);
+
+            expect(calls[6].args).toEqual(['rebound', 'Rebound Error 1']);
+            expect(calls[7].args).toEqual(['rebound', {}]);
+            expect(calls[8].args).toEqual(['rebound']);
+
+            expect(calls[9].args[0]).toEqual('end');
+        });
+    });
+
+
+
+    it('Should execute a Promise trigger and emit all events - data, end', function () {
+        var taskexec = new TaskExec();
+
+        taskexec.on('error', function(){});
+        spyOn(taskexec, 'emit').andCallThrough();
+
+        var module = require('./component/triggers/promise_trigger.js');
+
+        runs(() => {
+            taskexec.process(module, payload, cfg);
+        });
+
+        waitsFor(() => taskexec.emit.callCount > 1, 5000);
+
+        runs(() => {
+            expect(taskexec.emit).toHaveBeenCalled();
+            expect(taskexec.emit.calls[0].args[0]).toEqual('data');
+            expect(taskexec.emit.calls[0].args[1]).toEqual({
+                body: 'I am a simple promise'
+            });
+            expect(taskexec.emit.calls[1].args[0]).toEqual('end');
+        });
+    });
+
+
+    describe('Request Promise', function () {
+
+        beforeEach(function(){
+            nock('http://promise_target_url:80')
+                .get('/foo/bar')
+                .reply(200, {
+                    message: 'Life is good with promises'
+                });
+        });
+
+        it('Should execute a Promise trigger and emit all events - data, end', function () {
+
+
+            var taskexec = new TaskExec();
+
+            taskexec.on('error', function(){});
+            spyOn(taskexec, 'emit').andCallThrough();
+
+            var module = require('./component/triggers/promise_request_trigger.js');
+
+            runs(() => {
+                taskexec.process(module, payload, cfg);
+            });
+
+            waitsFor(() => taskexec.emit.callCount > 1, 5000);
+
+            runs(() => {
+                expect(taskexec.emit).toHaveBeenCalled();
+                expect(taskexec.emit.calls[0].args[0]).toEqual('data');
+                expect(taskexec.emit.calls[0].args[1]).toEqual({
+                    body:{
+                        message : 'Life is good with promises'
+                    }
+                });
+                expect(taskexec.emit.calls[1].args[0]).toEqual('end');
+            });
+        });
     });
 
 });
