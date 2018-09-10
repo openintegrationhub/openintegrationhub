d18765bd29c5e5c513c42f4896088d63cea86568
Falk Voigt <falk.voigt@basaas.com>
2018-09-10 15:17:54 +0200

    #72 rework build.js to build and upload independent

--- scripts/build.js
+++ scripts/build.js
@@ -6,17 +6,17 @@ if (services && services.length > 0) {
 
 
     services.forEach((service)=>{
+        const version = require(`../service/${service.name}/package.json`);
         console.log(service.name);
         // command for build needs to be defined
         execSync(`cd ${__dirname}/../services/${service.name}/ && echo $PWD`, { stdio: [0, 1, 2] });
-      });
+        // execSync('echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin', { stdio: [0, 1, 2] });
+        // execSync(`docker build -t $DOCKER_USERNAME/${service.name}:${version} .`, { stdio: [0, 1, 2] });
+        // execSync(`docker tag $DOCKER_USERNAME/${service.name}:$VERSION $DOCKER_USERNAME/${service.name}:latest`, { stdio: [0, 1, 2] });
+
+        // execSync(`docker push $DOCKER_USERNAME/${service.name}:$VERSION`, { stdio: [0, 1, 2] });
+        // execSync(`docker push $DOCKER_USERNAME/${service.name}:latest`, { stdio: [0, 1, 2] });
+    });
 } else {
     console.log('No Changes to Services!')
 }
-
-// echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-// docker build -t $DOCKER_USERNAME/${SERVICE_NAME}:$VERSION .
-// docker images
-// docker tag $DOCKER_USERNAME/${SERVICE_NAME}:$VERSION $DOCKER_USERNAME/${SERVICE_NAME}:latest
-// docker push $DOCKER_USERNAME/${SERVICE_NAME}:$VERSION
-// docker push $DOCKER_USERNAME/${SERVICE_NAME}:latest
