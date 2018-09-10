7c6eca5a3d56fa0ace4f41266caa64146b01484d
Falk Voigt <falk.voigt@basaas.com>
2018-09-10 14:53:50 +0200

    #72 fix some build.js stuff

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,14 @@
 
 const fs = require('fs');
 const { execSync } = require('child_process');
-const services = fs.readFileSync(`${__dirname}/toBuild.json`).toString('utf8');
-
+const services = require(`${__dirname}/toBuild.json`);
 if (services && services.length > 0) {
-    services.forEach(service, function(val) {
+
+
+    services.forEach((service)=>{
         console.log(service.name);
         // command for build needs to be defined
-        execSync(`cd ${__dirname}/../service/${service.name}/ && echo $PWD`, { stdio: [0, 1, 2] });
+        execSync(`cd ${__dirname}/../services/${service.name}/ && echo $PWD`, { stdio: [0, 1, 2] });
       });
 } else {
     console.log('No Changes to Services!')
