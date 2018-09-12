44663b8fdb33fbe873e53a6cf3690b3bfbbbbf22
Falk Voigt <falk.voigt@basaas.com>
2018-09-12 11:28:47 +0200

    #72 update deploy

--- scripts/install-gcloud.sh
+++ scripts/install-gcloud.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 set -e
-
-if [ ! -d ${HOME}/google-cloud-sdk ]; then
-    curl https://sdk.cloud.google.com | bash;
+if [ ! -d "$HOME/google-cloud-sdk/bin" ] 
+then 
+    rm -rf $HOME/google-cloud-sdk
+    export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+    curl https://sdk.cloud.google.com | bash
 fi
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
