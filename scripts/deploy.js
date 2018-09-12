0e7eb22302ccf70d006bfd18b7a1d3ea25495963
DennisCES <38039738+DennisCES@users.noreply.github.com>
2018-09-12 13:37:55 +0200

    Try to fix error in deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { execSync } = require('child_process');
+const exec = require('child_process').exec;
 const services = require(`${__dirname}/toBuild.json`);
 
 if (services && services.length > 0) {
