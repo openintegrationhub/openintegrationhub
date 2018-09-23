8f12d24f5a6d1cb35e28d762bde9e9963145da44
Anton Kotenko <anton@elastic.io>
2018-09-23 15:05:47 +0300

    openintegrationhub-32 make different services to be compatible in terms of build process

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,9 +8,10 @@ if (services && services.length > 0) {
         console.log('We going to Deploy Service: ',service.name);
         console.log('In the Version: ',service.version);
 
-        execSync (`${__dirname}/install-gcloud.sh`, { stdio: [0, 1, 2] });
         console.log('Deployment not yet enabled')
         console.log(`Command:  kubectl set image deployment/${service.name} ${service.name}=openintegrationhub/${service.name}:${service.version}`)
+
+        //execSync (`${__dirname}/install-gcloud.sh`, { stdio: [0, 1, 2] });
         // execSync (`kubectl set image deployment/${service.name} ${service.name}=openintegrationhub/${service.name}:${service.version}`, { stdio: [0, 1, 2] });
     });
 } else {
