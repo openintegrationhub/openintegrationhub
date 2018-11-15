06879957712ce6a0c88cf3162247b2ac9bcb8888
Selim Achmerzaev <selim.achmerzaev@basaas.com>
2018-11-15 14:29:05 +0100

    #134 initial master admin password in IAM

--- services/iam/src/util/check-env.js
+++ services/iam/src/util/check-env.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const log = require('@basaas/node-logger').getLogger('check-env', {
     level: 'info',
 });
@@ -13,4 +14,16 @@ module.exports.optional = (env, defaultValue) => {
     log.info(`Missing ${env}, using "${defaultValue}"`);
 
     return defaultValue;
+};
+
+module.exports.getPassword = (envVarName) => {
+    if (process.env[envVarName]) { return process.env[envVarName]; }
+
+    const password = crypto.randomBytes(12).toString('base64');
+    log.warn(`Missing ${envVarName}. This password will be auto-generated only displayed once via stdout!\n
+    ===============================================\n
+    || Auto generated password: ${password} ||\n
+    ===============================================\n`);
+
+    return password;
 };
