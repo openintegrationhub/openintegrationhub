ec1c9408052e522561ad7f67874234e71c83ec11
Pavel Nedelko <pnedelko@gmail.com>
2018-12-17 14:03:02 +0100

    fixed tests

--- src/resource-coordinator/src/dao/FlowsDao.js
+++ src/resource-coordinator/src/dao/FlowsDao.js
@@ -1,21 +1,21 @@
 class FlowsDao {
-    async findById(id) {
+    async findById(id) { //eslint-disable-line
         throw new Error('To be implemented');
     }
 
-    async update(flow) {
+    async update(flow) { //eslint-disable-line
         throw new Error('To be implemented');
     }
 
-    async findAll() {
+    async findAll() { //eslint-disable-line
         throw new Error('To be implemented');
     }
 
-    async ensureFinalizer(flow) {
+    async ensureFinalizer(flow) { //eslint-disable-line
         throw new Error('To be implemented');
     }
 
-    async removeFinalizer(flow) {
+    async removeFinalizer(flow) { //eslint-disable-line
         throw new Error('To be implemented');
     }
 }
