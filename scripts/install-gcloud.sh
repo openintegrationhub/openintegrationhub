12611a50758d7bb883491820554f9c2d520f648b
SvenHoeffler <shoeffler@wice.de>
2019-01-21 10:33:47 +0100

    Testing CircleCI deploy

--- scripts/install-gcloud.sh
+++ scripts/install-gcloud.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-if [ ! -d "$HOME/google-cloud-sdk/bin" ] 
-then 
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]
+then
     rm -rf $HOME/google-cloud-sdk
     export CLOUDSDK_CORE_DISABLE_PROMPTS=1
     curl https://sdk.cloud.google.com | bash > /dev/null
@@ -13,11 +13,11 @@ echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project galvanic-veld-209811
-gcloud --quiet config set container/cluster oih-production-v1	
+gcloud --quiet config set container/cluster oih-production-v1
 gcloud --quiet config set compute/zone europe-west3-c
 gcloud components install kubectl
-gcloud --quiet container clusters get-credentials oih-production-v1	
-
+gcloud --quiet container clusters get-credentials oih-production-v1
+echo 'Get Access to Cluster'
 kubectl config view
+echo 'Set Default Cluster Context'
 kubectl config current-context
-
