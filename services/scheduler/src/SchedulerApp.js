9ea4c7532d4c1c30f8774d3b00b54101b4e8a83b
Pavel Nedelko <pnedelko@gmail.com>
2019-02-13 13:59:11 +0100

    Scheduler POC

--- services/scheduler/src/SchedulerApp.js
+++ services/scheduler/src/SchedulerApp.js
@@ -7,21 +7,60 @@ const { Scheduler } = require('@openintegrationhub/scheduler');
 const FlowsDao = require('./FlowsDao');
 const SchedulePublisher = require('./SchedulePublisher');
 const { asValue, asClass } = require('awilix');
+const { EventBus, RabbitMqTransport } = require('@openintegrationhub/event-bus');
+const mongoose = require('mongoose');
+const Flow = require('./models/Flow');
 
 class SchedulerApp extends App {
     async _run() {
         const container = this.getContainer();
         const config = container.resolve('config');
+        const logger = container.resolve('logger');
         const amqp = container.resolve('amqp');
-        const k8s = container.resolve('k8s');
+        // const k8s = container.resolve('k8s');
         await amqp.start();
-        await k8s.start();
+        // await k8s.start();
         this._initHealthcheckApi(config.get('LISTEN_PORT'));
         const channel = await amqp.getConnection().createChannel();
         const queueCreator = new QueueCreator(channel);
 
+        await mongoose.connect(config.get('MONGODB_URI'), {useNewUrlParser: true});
+
+        const transport = new RabbitMqTransport({rabbitmqUri: config.get('RABBITMQ_URI')});
+        const eventBus = new EventBus({transport, serviceName: this.constructor.NAME});
+        await eventBus.connect();
+
+        //@todo: provide some specific topic
+        await eventBus.subscribe({topic: '*.*'}, async (event) => {
+            logger.trace({event}, 'Received event');
+
+            try {
+                let flow = await Flow.findById(event.payload.id);
+                if (!flow) {
+                    flow = new Flow({_id: event.payload.id});
+                }
+                if (!event.payload.cron) {
+                    event.payload.cron = undefined; // in order to delete the cron field
+                }
+                Object.assign(flow, event.payload);
+
+                if (flow.cron) {
+                    //@todo: some validation?
+                    flow.updateDueExecutionAccordingToCron();
+                    //@todo: delete if wrong status
+                    await flow.save();
+                } else {
+                    await Flow.deleteOne({_id: flow.id});
+                }
+                await event.ack();
+            } catch (err) {
+                logger.error({ err, event }, 'Unable to process event');
+                await event.nack();
+            }
+        });
+
         container.register({
-            crdClient: asValue(k8s.getCRDClient()),
+            // crdClient: asValue(k8s.getCRDClient()),
             channel: asValue(channel),
             queueCreator: asValue(queueCreator),
             flowsDao: asClass(FlowsDao),
