7fc5c3f05a3f3376242f01571d2c56ebc4af1987
SvenHoeffler <shoeffler@wice.de>
2019-02-25 09:34:57 +0100

    Switched to new naming scheme

--- services/flow-repository/app/config/index.js
+++ services/flow-repository/app/config/index.js
@@ -1,8 +1,6 @@
 // General configuration file for variable urls, settings, etc.
 
 const general = {
-  iamBaseUrl: process.env.IAM_BASE_URL || 'http://localhost:3099',
-  iamApiBaseUrl: process.env.IAM_API_BASE_URL || 'http://localhost:3099',
   mongoUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/icrdev',
   oihViewerRoles: ['ADMIN'],
   tenantWriterRoles: ['TENANT_ADMIN', 'TENANT_INTEGRATOR'],
