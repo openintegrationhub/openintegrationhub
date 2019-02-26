542d7b115ef569c427210a791e991ec6c8843815
Pavel Nedelko <pnedelko@gmail.com>
2019-02-26 09:49:16 +0100

    fixes model test

--- services/resource-coordinator/spec/models/Flow.spec.js
+++ services/resource-coordinator/spec/models/Flow.spec.js
@@ -54,13 +54,4 @@ describe('Flow model', () => {
             expect(flow.getFirstNode()).to.be.undefined;
         });
     });
-
-    describe('isDeleted', () => {
-        it('should return true', () => {
-            const flow = new Flow({
-                status: 'stopping'
-            });
-            expect(flow.isDeleted).to.be.true;
-        });
-    });
 });
