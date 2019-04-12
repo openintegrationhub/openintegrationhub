f634d3183a344855f92aa0ed938c8a6bc35d3383
DennisCES <38039738+DennisCES@users.noreply.github.com>
2019-04-12 13:39:30 +0200

    Create tests.js

---
+++ services/tests.js
@@ -0,0 +1,31 @@
+process.env.AUTH_TYPE = 'basic';
+
+const request = require('supertest')('http://iam.openintegrationhub.com/login');
+const username = process.env.username;
+const password = process.env.password;
+
+let conf = null;
+
+describe('User Routes', () => {
+
+    let tokenUser = null; 
+    let tokenAdmin = null;
+    let app = null;
+    
+    beforeAll(async (done) => {
+        process.env.IAM_AUTH_TYPE = 'basic';
+        setTimeout(async () => {
+
+            const jsonPayload = {
+                username,
+                password,
+            };
+            const response = await request.post('/login')
+                .send(jsonPayload)
+                .set('Accept', '\/application\/json\/')
+                .expect(200);
+            tokenAdmin = `Bearer ${response.body.token}`;
+                    done();
+        }, 200); //timeout
+    });
+});
