87c0da0474fddc7a9b8603ae682581a46668454f
DennisCES <38039738+DennisCES@users.noreply.github.com>
2019-04-16 17:08:22 +0200

    Update test.spec.js

--- services/integrationtests/test.spec.js
+++ services/integrationtests/test.spec.js
@@ -38,7 +38,8 @@ describe('User Routes', () => {
             method: 'GET',
             uri: `http://flow-repository.openintegrationhub.com/flows`,
             headers: {
-                authorization: `Bearer ${tokenAdmin}`,
+                //authorization: `Bearer ${tokenAdmin}`,
+                header.set("Authorization :", "Bearer " + tokenAdmin);
             },
         };
          
