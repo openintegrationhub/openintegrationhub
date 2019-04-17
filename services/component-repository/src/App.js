fed391f46c4cc94ccb62d5986551f39b888983f5
Pavel Nedelko <pnedelko@gmail.com>
2019-04-17 17:10:34 +0200

    added service/component-repository

--- services/component-repository/src/App.js
+++ services/component-repository/src/App.js
@@ -1,14 +1,19 @@
 const { App } = require('backend-commons-lib');
+const { Server } = require('@openintegrationhub/component-repository');
 
 class ComponentRepositoryApp extends App {
     async _run() {
-        const { asValue, asClass, asFunction } = this.awilix;
+        const { asClass} = this.awilix;
         const container = this.getContainer();
-        const config = container.resolve('config');
 
         container.register({
-
+            server: asClass(Server)
+                .singleton()
+                .inject(() => ({iam: undefined})) //use default iam middleware
         });
+
+        const server = container.resolve('server');
+        await server.start();
     }
 
     static get NAME() {
