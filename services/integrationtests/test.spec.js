b6ceabeb15d5e29c966c57390db1fd9039057476
DennisCES <38039738+DennisCES@users.noreply.github.com>
2019-04-30 12:07:33 +0200

    Update test.spec.js

--- services/integrationtests/test.spec.js
+++ services/integrationtests/test.spec.js
@@ -238,7 +238,7 @@ describe('User Routes', () => {
 				}
 		};
 	const response = await request(getAllLogs);
-	console.log(response);
+	console.log(JSON.stringify(response.body);
 	expect(response.statusCode).toEqual(200);
 	done();
 	});
