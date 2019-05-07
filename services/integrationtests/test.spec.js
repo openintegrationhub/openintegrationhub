0b028fd2877b09fb95dfb7b362813090b32b2d59
DennisCES <38039738+DennisCES@users.noreply.github.com>
2019-05-07 10:33:06 +0200

    Update test.spec.js

--- services/integrationtests/test.spec.js
+++ services/integrationtests/test.spec.js
@@ -205,7 +205,7 @@ describe('User Routes', () => {
 		
 		var start = new Date().getTime();
 		for (var i = 0; i < 1e7; i++) {
-		  if ((new Date().getTime() - start) > 5000){
+		  if ((new Date().getTime() - start) > 10000){
 			break;
 		  }
 		}
