e6ac78318ef159a879670f1915145a9b6d0a371d
Hans Eggert <hans.eggert@basaas.com>
2019-05-22 11:59:34 +0200

    init web ui

---
+++ services/web-ui/src/container/app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
+
+export default App;
