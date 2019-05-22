7448aff79f5b12b3f8c279b7c74e4d12db0c88a4
Falk Voigt <falk.voigt@basaas.com>
2019-05-22 14:29:02 +0200

    add login container

---
+++ services/web-ui/src/container/login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+import './index.css';
+
+function Login() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary"/>
+    </div>
+  );
+}
+
+export default Login;
