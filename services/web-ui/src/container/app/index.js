0cd79147b3cca2887c5c246a37d65b37e105d288
Hans Eggert <hans.eggert@basaas.com>
2019-05-23 11:33:16 +0200

    Add favicon and page title

--- services/web-ui/src/container/app/index.js
+++ services/web-ui/src/container/app/index.js
@@ -10,8 +10,8 @@ import LoginCheck from '../../component/login-check';
 import './index.css';
 
 function App() {
+    document.title = 'Web UI';
     return (
-
         <Switch>
             <Route exact path="/auth" component={Auth} />
             <LoginCheck>
@@ -19,8 +19,6 @@ function App() {
                 <Route exact path="/" component={Home} />
             </LoginCheck>
         </Switch>
-
-
     );
 }
 
