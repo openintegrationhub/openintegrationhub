8fe7ad7601d39597df9b52a8a31dc966cbf799d2
Hans Eggert <hans.eggert@basaas.com>
2019-05-23 09:26:22 +0200

    Add app conf

--- services/web-ui/src/index.js
+++ services/web-ui/src/index.js
@@ -1,22 +1,29 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import App from './container/app';
+import { updateConfig } from './conf';
 
 import configureStore, { history } from './store';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <Provider store={configureStore()}>
-        <Router history={history}>
 
-            <App />
-        </Router>
-    </Provider>,
-    document.getElementById('root'),
-);
+(async () => {
+    updateConfig((await axios.get('/config')).data);
+    ReactDOM.render(
+        <Provider store={configureStore()}>
+            <Router history={history}>
+
+                <App />
+            </Router>
+        </Provider>,
+        document.getElementById('root'),
+    );
+})();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- services/web-ui/src/setupProxy.js
+++ services/web-ui/src/setupProxy.js
@@ -1,6 +1,12 @@
 const proxy = require('http-proxy-middleware');
+const conf = require('../server/conf');
 
 // eslint-disable-next-line func-names
 module.exports = function (app) {
+    // proxy server config
+    app.use('/config', (req, res) => {
+        res.send(conf);
+    });
+
     app.use(proxy('/login', { target: 'http://iam.openintegrationhub.com', changeOrigin: true }));
 };
