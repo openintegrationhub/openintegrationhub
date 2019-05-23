3a0bb0356c1568607abd2bdcd28c7367d018238f
Hans Eggert <hans.eggert@basaas.com>
2019-05-23 13:35:02 +0200

    Add grid

--- services/web-ui/src/component/user/index.js
+++ services/web-ui/src/component/user/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import { withStyles } from '@material-ui/styles';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import Grid from '@material-ui/core/Grid';
+import flow from 'lodash/flow';
 
-import './index.css';
+const useStyles = {
+    loginContainer: {
+        flexGrow: 1,
+    },
+    form: {
+        float: 'none',
+        margin: 'auto',
+        padding: '40vh 0',
+        width: 200,
+    },
+    frame: {
+        height: '100vh',
+    },
+    formGroup: {
+        padding: '30px 0 0 0 ',
+    },
+};
 
 class User extends React.Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div className="App">
-              Usermanagement
+            <div className={classes.loginContainer}>
+                <Grid container >
+                    <Grid item xs={4}></Grid>
+                    <Grid item xs={4}>
+                        <div className={classes.frame}>
+                            adsfsdf
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}></Grid>
+                </Grid>
+
             </div>
         );
     }
 }
 
-export default User;
+export default flow(
+    withStyles(useStyles),
+)(User);
