2173f84ba45bd5299beb291ccd62e88c438be791
Shterion <shterion.yanev@yahoo.com>
2019-07-23 14:35:38 +0200

    Update jest conf

--- services/ils/jest.config.js
+++ services/ils/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   globalSetup: './test/utils/setup.js',
   globalTeardown: './test/utils/teardown.js',
   testEnvironment: './test/utils/mongo-environment.js',
-  testRegex: "./test/test.*.js$",
+  testRegex: "(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
   collectCoverageFrom: [
     'app/**/*.{js}',
     '!<rootDir>/app/config/logger.js',
