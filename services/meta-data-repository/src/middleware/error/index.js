d2c7ed2c4037e43fccd33669baf0d1fbe3db82a0
Hans Eggert <hans.eggert@basaas.com>
2019-08-07 11:36:14 +0200

    Change error log

--- services/meta-data-repository/src/middleware/error/index.js
+++ services/meta-data-repository/src/middleware/error/index.js
@@ -1,6 +1,15 @@
 const defaultErrorHandler = (errObj, req, res, next) => { // eslint-disable-line
     const status = errObj.status;
-    const message = errObj.err || 'Error';
+    let message = 'Error';
+
+    if (errObj.err) {
+        if (errObj.err.message) {
+            message = errObj.err.message;
+        } else {
+            message = errObj.err;
+        }
+    }
+
     res.status(status);
     res.send({
         message,
