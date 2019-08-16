9f89008f39992e91f6c67f7aaaa5a118941cbfde
Falk Voigt <falk.voigt@basaas.com>
2019-08-16 11:34:08 +0200

    Fix tests

--- services/web-ui/src/component/roles/roles-teaser/index.test.js
+++ services/web-ui/src/component/roles/roles-teaser/index.test.js
@@ -8,7 +8,9 @@ it('renders without crashing', () => {
     mount(
         <Provider store={global.configureStore()}>
             <MemoryRouter>
-                <Component data={ { name: 'Test', description: 'TEST', isGlobal: true } } />
+                <Component data={ {
+                    name: 'Test', description: 'TEST', isGlobal: true, permissions: [],
+                } } />
             </MemoryRouter>
         </Provider>,
     );
