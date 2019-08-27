03f100cd46278d03bf698cdd15538552e032eee0
Falk Voigt <falk.voigt@basaas.com>
2019-08-27 13:48:46 +0200

    fix Tests

--- services/web-ui/src/component/profile/index.test.js
+++ services/web-ui/src/component/profile/index.test.js
@@ -6,7 +6,36 @@ import Component from '.';
 
 it('renders without crashing', () => {
     mount(
-        <Provider store={global.configureStore()}>
+        <Provider store={global.configureStore({
+            auth: {
+                _id: 'sadferhrwthasdg',
+            },
+            users: {
+                all: [
+                    {
+                        _id: 'sadferhrwthasdg',
+                        isLoggedIn: true,
+                        username: 'test@test.de',
+                        tenant: '123456789',
+                        firstname: 'test',
+                        lastname: 'test',
+                        permissions: ['all'],
+                    },
+                ],
+            },
+            tenants: {
+                all: [
+                    {
+                        _id: '123456789',
+                        isLoggedIn: true,
+                        username: 'test@test.de',
+                        firstname: 'test',
+                        lastname: 'test',
+                        permissions: ['all'],
+                    },
+                ],
+            },
+        })}>
             <MemoryRouter>
                 <Component />
             </MemoryRouter>
