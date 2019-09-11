48dc7e371ff59bef88d39f42c34722a0661960af
SvenHoeffler <shoeffler@wice.de>
2019-09-11 13:29:15 +0200

    endpoint now calls create dummy queue function

--- services/dispatcher-service/app/api/controllers/dispatches.js
+++ services/dispatcher-service/app/api/controllers/dispatches.js
@@ -2,9 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const log = require('../../utils/logger');
 const storage = require('../../utils/mongo');
+const { createDummyQueues } = require('../../utils/eventBus');
 
 const jsonParser = bodyParser.json();
 const router = express.Router();
+
+function getKeys(connections) {
+  const keys = [];
+  for (let i = 0; i < connections.length; i += 1) {
+    const { targets } = connections[i];
+    for (let j = 0; j < targets.length; j += 1) {
+      keys.push(`dispatch.${targets[j].flowId}`);
+    }
+  }
+  return keys;
+}
 
 router.get('/', jsonParser, async (req, res) => {
   try {
@@ -23,12 +35,16 @@ router.get('/', jsonParser, async (req, res) => {
 
 router.put('/', jsonParser, async (req, res) => {
   try {
+    const connections = req.body;
     const configuration = {
-      connections: req.body,
+      connections,
     };
     configuration.tenant = req.user.tenant;
 
     const response = await storage.upsertConfig(configuration);
+
+    const keys = getKeys(connections);
+    await createDummyQueues(keys);
 
     return res.status(201).send({ meta: {}, data: response });
   } catch (e) {
