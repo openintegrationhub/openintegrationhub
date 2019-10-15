6209bf3b21a917c784805ecffccd1a84aa31e81d
Selim Achmerzaev <selim.achmerzaev@basaas.com>
2019-10-15 11:01:52 +0200

    #778: Base implementation of app directory

---
+++ services/app-directory/src/model/App.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const component = new Schema({
+    type: { type: String, index: true, required: true },
+    name: { type: String, index: true, required: true },
+    uri: { type: String },
+}, { _id: false });
+
+const app = new Schema({
+    artifactId: {
+        type: String, lowercase: true, index: { unique: true }, required: true,
+    },
+    name: { type: String, index: { unique: true }, required: true },
+    description: { type: String },
+    img: { type: String },
+    dataModels: [String],
+    components: [component],
+    isGlobal: Boolean,
+    tenant: String,
+    urls: {
+        main: String,
+    },
+    status: { type: String },
+    supportedConnections: {
+        inbound: Boolean,
+        outbound: Boolean,
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('app', app);
