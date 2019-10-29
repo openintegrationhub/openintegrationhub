2d9f96ccd5488f270a26575a32258fdb70e5afe0
Pavel Nedelko <pnedelko@gmail.com>
2019-10-29 17:54:56 +0100

    Snapshots service WIP (#841)
    * introduced snapshots service
    
    * printing message properties
    
    * fixed bugs after c&p

---
+++ services/snapshots-service/src/routes/snapshots/index.ts
@@ -0,0 +1,19 @@
+import KoaRouter from 'koa-router';
+import Controller from './controller';
+import { RouterContext } from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import { parsePagedQuery } from './middlewares';
+import { koaMiddleware } from '@openintegrationhub/iam-utils';
+
+export default () => {
+    const controller = new Controller();
+    return new KoaRouter()
+        .use(bodyParser())
+        .use(koaMiddleware)
+        .get('/', parsePagedQuery(), (ctx: RouterContext) => controller.getMany(ctx))
+        .post('/', (ctx: RouterContext) => controller.postOne(ctx))
+        .get('/:id', (ctx: RouterContext) => controller.getOne(ctx))
+        .put('/:id', (ctx: RouterContext) => controller.putOne(ctx))
+        .patch('/:id', (ctx: RouterContext) => controller.patchOne(ctx))
+        .routes();
+}
