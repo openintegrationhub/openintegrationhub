f6ce1ef22492b7bb6d3713326cc4cd525c7fe02a
Pavel Nedelko <pnedelko@gmail.com>
2019-11-06 14:31:09 +0100

    bootstraped the logging service (#863)

---
+++ services/logging-service/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports =  {
+    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+    extends:  [
+        'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    ],
+    parserOptions:  {
+        ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+        sourceType:  'module',  // Allows for the use of imports
+    },
+    rules:  {
+        '@typescript-eslint/no-angle-bracket-type-assertion': 'off',
+        '@typescript-eslint/interface-name-prefix': ['error', 'always']
+    },
+};
---
+++ services/logging-service/src/routes/logs/index.ts
@@ -0,0 +1,20 @@
+import KoaRouter from 'koa-router';
+import Controller from './controller';
+import { RouterContext } from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import { koaMiddleware } from '@openintegrationhub/iam-utils';
+import { ConfigStore, Logger } from 'backend-commons-lib';
+
+interface ILogsRouterOptions {
+    config: ConfigStore;
+    logger: Logger;
+}
+
+export default ({config, logger}: ILogsRouterOptions) => {
+    const controller = new Controller({config, logger});
+    return new KoaRouter()
+        .use(bodyParser())
+        .use(koaMiddleware)
+        .get('/flows/:flowId/steps/:stepId', (ctx: RouterContext) => controller.getOne(ctx))
+        .routes();
+}
