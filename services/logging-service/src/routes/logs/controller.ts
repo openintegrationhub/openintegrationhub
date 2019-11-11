e172c540a52f3764c71551c4e3c1ee459692fde1
Pavel Nedelko <pnedelko@gmail.com>
2019-11-11 16:33:14 +0100

    added paging to the logging service

--- services/logging-service/src/routes/logs/controller.ts
+++ services/logging-service/src/routes/logs/controller.ts
@@ -1,10 +1,21 @@
 import { RouterContext } from 'koa-router';
-import { Logging } from '@google-cloud/logging';
+import {Entry, Logging} from '@google-cloud/logging';
 import {ConfigStore, Logger} from 'backend-commons-lib';
+import BadRequest from '../../errors/api/BadRequest';
 
 interface ILogsControllerOptions {
     config: ConfigStore;
     logger: Logger;
+}
+
+interface IGetLogsParams {
+    flowId: string;
+    stepId: string;
+}
+
+interface IGetLogsQuery {
+    pageSize?: string;
+    pageToken?: string;
 }
 
 export default class LogsController {
@@ -16,22 +27,36 @@ export default class LogsController {
         this.config = config;
         this.logger = logger;
         this.gcloudLogging = new Logging({
-            keyFilename: config.get('GOOGLE_APPLICATION_CREDENTIALS'),
-            credentials: config.get('GCLOUD_CREDENTIALS') // credentials as JSON
+            keyFilename: config.get('GOOGLE_APPLICATION_CREDENTIALS')
         });
     }
 
     public async getOne(ctx: RouterContext): Promise<void> {
-        const { flowId, stepId } = ctx.params;
+        const { flowId, stepId } = <IGetLogsParams>ctx.params;
+        let { pageSize = 1000, pageToken } = <IGetLogsQuery>ctx.query;
+
+        pageSize = parseInt(pageSize as string, 10);
+        if (Number.isNaN(pageSize)) {
+            throw new BadRequest('pageSize should be a valid number');
+        }
+
         const options = {
-            pageSize: 10,
+            pageSize,
+            pageToken,
             filter: `resource.type=k8s_container AND labels.k8s-pod/stepId=${stepId} AND labels.k8s-pod/flowId=${flowId}`
         };
 
-        const [entries] = await this.gcloudLogging.getEntries(options);
+        const [entries, request, response] = await this.gcloudLogging.getEntries(options);
 
         ctx.body = {
-            data: entries
+            data: entries ? entries.map((entry: Entry) => ({
+                timestamp: entry.metadata.timestamp,
+                message: entry.data
+            })) : [],
+            meta: {
+                pageSize,
+                nextPageToken: response.nextPageToken
+            }
         };
     }
 }
