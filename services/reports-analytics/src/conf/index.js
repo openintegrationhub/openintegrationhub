0326d439ff0bef80e6f5f7dbacbea3dbf2a2ce01
Hans Eggert <hans.eggert@basaas.com>
2019-11-22 13:53:10 +0100

    Update code

--- services/reports-analytics/src/conf/index.js
+++ services/reports-analytics/src/conf/index.js
@@ -8,6 +8,10 @@ module.exports = {
         namespace: optional('LOG_NAMESPACE', name),
         level: optional('LOG_LEVEL', 'error'),
     },
+    grafana: {
+        url: optional('GRAFANA_URL', 'http://127.0.0.1:8888'),
+        token: optional('GRAFANA_TOKEN', ''),
+    },
     mongoDbConnection: optional('MONGODB_CONNECTION', 'mongodb://localhost:27017/reports-analytics?replicaSet=rs'),
     influxDatabase: optional('INFLUXDB_DATABASE', 'statistics'),
     debugMode: optional('DEBUG_MODE', 'false') === 'true',
--- services/reports-analytics/src/server/index.js
+++ services/reports-analytics/src/server/index.js
@@ -3,23 +3,15 @@ const {
 } = require('@openintegrationhub/event-bus');
 const mongoose = require('mongoose');
 const logging = require('@basaas/node-logger');
+const express = require('express');
 const Influx = require('influx');
 const conf = require('../conf');
 const Measurement = require('../dao/measurement');
+const defaultMeasurement = require('../measurement/default');
 const { getValidMeasurements } = require('../validate');
 const { transformMeasurement, transformEntries } = require('../transform');
 
 const logger = logging.getLogger(`${conf.log.namespace}/server`);
-
-const defaultSchema = {
-    measurement: 'default',
-    fields: {
-        keyDefault: Influx.FieldType.STRING,
-    },
-    tags: [
-        'event_name',
-    ],
-};
 
 async function createCollections() {
     try {
@@ -47,16 +39,18 @@ module.exports = class Server {
         });
     }
 
+    async setupExpress() {
+        this.app = express();
+        this.app.disable('x-powered-by');
+        this.app.use('/grafana-proxy', require('../route/grafana-proxy'));
+    }
+
     async setupTimeseries(schemas) {
-        console.log([
-            defaultSchema,
-            ...schemas,
-        ]);
         this.influx = new Influx.InfluxDB({
             host: process.env.INFLUX_HOST,
             database: conf.influxDatabase,
             schema: [
-                defaultSchema,
+                defaultMeasurement,
                 ...schemas,
             ],
         });
@@ -76,7 +70,6 @@ module.exports = class Server {
 
         this.eventBus.subscribe('#', async (event) => {
             try {
-                console.log(event.name);
                 await this.handleEvent(event);
             } catch (err) {
                 logger.error(err);
@@ -91,16 +84,17 @@ module.exports = class Server {
     async writeTs(event, measurement) {
         try {
             const payload = [];
-
             // add default measurement
             payload.push({
-                measurement: defaultSchema.measurement,
+                measurement: defaultMeasurement.measurement,
                 fields: {
                     keyDefault: '.',
                 },
                 tags: {
                     event_name: event.name,
                 },
+                // use event creation time in nanoseconds
+                timestamp: `${event.createdAt}000000`,
             });
 
             if (measurement) {
@@ -109,9 +103,10 @@ module.exports = class Server {
                     measurement: measurement.measurementName,
                     fields: transformEntries(measurement.payloadMapping.fields, event.payload),
                     tags: transformEntries(measurement.payloadMapping.tags, event.payload),
+                    // use event creation time in nanoseconds
+                    timestamp: `${event.createdAt}000000`,
                 });
             }
-            console.log(`write ${  event.name}`);
             await this.influx.writePoints(payload);
         } catch (err) {
             logger.error(err);
@@ -127,6 +122,10 @@ module.exports = class Server {
             // setup database
             await this.setupDatabase();
             await createCollections();
+
+            // init express
+            await this.setupExpress();
+            this.server = await this.app.listen(conf.port);
 
             // load valid measuremnts
             this.measurements = getValidMeasurements(await Measurement.get());
@@ -151,6 +150,11 @@ module.exports = class Server {
 
     async stop() {
         mongoose.connection.close();
+        if (this.server) {
+            mongoose.connection.close();
+            this.server.close();
+        }
+
         if (this.eventBus) {
             await this.eventBus.disconnect();
         }
