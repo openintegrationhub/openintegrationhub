eeecd1b2064842780f8ca36c3e44df6ee7e9c2c5
Anton Kotenko <anton-kotenko@users.noreply.github.com>
2020-05-26 15:58:55 +0300

    elasticio-4034  bugfix (#143)
    Co-authored-by: Anton Kotenko <anton@elastic.io>

--- mocha_spec/unit/executor.spec.js
+++ mocha_spec/unit/executor.spec.js
@@ -1,5 +1,7 @@
 const chai = require('chai');
 const { expect } = chai;
+const sinon = require('sinon');
+chai.use(require('sinon-chai'));
 
 const TaskExec = require('../../lib/executor.js').TaskExec;
 
@@ -59,6 +61,92 @@ describe('Executor', () => {
         it('should return config', () => {
             const taskExec = new TaskExec(defaultTaskExecArgs);
             expect(taskExec.__getAmqp()).to.equal(defaultTaskExecArgs.services.amqp);
+        });
+    });
+    describe('Return Promise interface', () => {
+        describe('error is thrown', () => {
+            it('should emit error and end with await\'s', (done) => {
+                const taskExec = new TaskExec(defaultTaskExecArgs);
+                const onDataStub = sinon.stub();
+                const error = new Error();
+                const emitErrorDoneStub = sinon.stub();
+                const onErrorStub = sinon.stub().callsFake(async () => new Promise(resolve => process.nextTick(() => {
+                    // move emitErrorDoneStub to next tick
+                    // to make possible to test that emit('end') really wait's for job
+                    emitErrorDoneStub();
+                    resolve();
+                })));
+                const onEndStub = sinon.stub().callsFake(async () => {
+                    try {
+                        expect(onErrorStub).to.have.been.calledOnce.and.calledWith(error);
+                        expect(onEndStub).to.have.been
+                            .calledAfter(onErrorStub)
+                            .and.calledAfter(emitErrorDoneStub)
+                            .and.calledOnce;
+                        expect(onDataStub).not.to.have.been.called;
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                const triggerOrActionStub = sinon.stub().rejects(error);
+                const payload = {};
+                const cfg = {};
+                const snapshot = {};
+                taskExec
+                    .on('error', onErrorStub)
+                    .on('end', onEndStub)
+                    .on('data', onDataStub);
+
+                taskExec.process(
+                    { process: triggerOrActionStub },
+                    payload,
+                    cfg,
+                    snapshot
+                );
+            });
+        });
+        describe('promise is returned', () => {
+            it('should emit error and end with await\'s', (done) => {
+                const taskExec = new TaskExec(defaultTaskExecArgs);
+                const onErrorStub = sinon.stub();
+                const emitDataDoneStub = sinon.stub();
+                const data = {};
+                const onDataStub = sinon.stub().callsFake(async () => new Promise(resolve => process.nextTick(() => {
+                    // move emitDataDoneStub to next tick
+                    // to make possible to test that emit('end') really wait's for job
+                    emitDataDoneStub();
+                    resolve();
+                })));
+                const onEndStub = sinon.stub().callsFake(async () => {
+                    try {
+                        expect(onDataStub).to.have.been.calledOnce.and.calledWith(data);
+                        expect(onEndStub).to.have.been
+                            .calledAfter(onDataStub)
+                            .and.calledAfter(emitDataDoneStub)
+                            .and.calledOnce;
+                        expect(onErrorStub).not.to.have.been.called;
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                const triggerOrActionStub = sinon.stub().resolves(data);
+                const payload = {};
+                const cfg = {};
+                const snapshot = {};
+                taskExec
+                    .on('error', onErrorStub)
+                    .on('end', onEndStub)
+                    .on('data', onDataStub);
+
+                taskExec.process(
+                    { process: triggerOrActionStub },
+                    payload,
+                    cfg,
+                    snapshot
+                );
+            });
         });
     });
 });
