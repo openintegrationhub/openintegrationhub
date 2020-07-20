dbecd3a9b0d345335a0da94efd8cb53cb2cdfda4
heggert <hans@eggert.link>
2020-07-21 01:20:49 +0200

    Publish to orchestrator instead of direct publishing to first node/component

--- services/scheduler/src/SchedulePublisher.js
+++ services/scheduler/src/SchedulePublisher.js
@@ -1,47 +1,24 @@
-const uuid = require('uuid/v1');
 const { SchedulePublisher } = require('@openintegrationhub/scheduler');
+const { Event } = require('@openintegrationhub/event-bus')
 
 class OIH_SchedulePublisher extends SchedulePublisher {
-    constructor({ logger, queueCreator, channel }) {
+    constructor({ logger, queueCreator, channel, eventBus }) {
         super();
         this._logger = logger;
         this._queueCreator = queueCreator;
         this._channel = channel;
+        this._eventBus = eventBus
     }
-
-    async scheduleFlow(flow) { //eslint-disable-line no-unused-vars
-        this._logger.trace({ flowId: flow.id }, 'schedule flow tick');
-
-        const scheduleRecord = {
-            'taskId': flow.id,
-            'execId': uuid().replace(/-/g, ''),
-            'userId': 'DOES NOT MATTER'
-        };
-
-        //@todo: introduce common Message class
-        const msg = {
-            id: uuid(),
-            attachments: {},
-            body: {},
-            headers: {},
-            metadata: {}
-        };
-
-        const {
-            messagesQueue,
-            exchangeName,
-            deadLetterRoutingKey
-        } = this._queueCreator.getAmqpStepConfig(flow, flow.getFirstNode().id);
-
-        await this._queueCreator.assertMessagesQueue(messagesQueue, exchangeName, deadLetterRoutingKey);
-
-        await this._channel.sendToQueue(
-            this._queueCreator.getAmqpStepConfig(flow, flow.getFirstNode().id).messagesQueue,
-            Buffer.from(JSON.stringify(msg)),
-            {
-                headers: scheduleRecord
+    async scheduleFlow(flow) {
+        const event = new Event({
+            headers: {
+                name: 'flow.execute'
+            },
+            payload: {
+                flow
             }
-        );
+        });
+        this._eventBus.publish(event)
     }
 }
 
