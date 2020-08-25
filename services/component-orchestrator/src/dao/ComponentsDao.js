68076fdb248c21fb65a85a1e81af9318fef7724f
heggert <hans@eggert.link>
2020-08-25 18:02:02 +0200

    Add lru cache

--- services/component-orchestrator/src/dao/ComponentsDao.js
+++ services/component-orchestrator/src/dao/ComponentsDao.js
@@ -1,4 +1,5 @@
 const { ComponentsDao } = require('@openintegrationhub/component-orchestrator');
+const LRU = require('lru-cache')
 const request = require('request');
 const { URL } = require('url');
 const path = require('path');
@@ -11,10 +12,24 @@ class OIHComponentsDao extends ComponentsDao {
         super();
         this._config = config;
         this._logger = logger;
+
+        this._cache = new LRU({
+            max: this._config.get('CACHE_COMPONENT_SIZE') || 50,
+            maxAge: this._config.get('CACHE_COMPONENT_MAX_AGE') || 1000 * 60 * 5
+        })
     }
 
     async findById(compId) {
+
+        const cachedComponent = this._cache.get(compId)
+
+        if (cachedComponent) {
+            this._logger.info({ compId }, 'Returning cached component');
+            return cachedComponent;
+        }
+
         const url = this._getComponentRepoUrl(`/components/${compId}`);
+
         const opts = {
             url,
             json: true,
@@ -22,11 +37,14 @@ class OIHComponentsDao extends ComponentsDao {
                 authorization: `Bearer ${this._config.get('IAM_TOKEN')}`
             }
         };
-        this._logger.trace({ compId }, 'Fetching component info');
+
+        this._logger.info({ compId }, 'Fetching component info');
         const { body, statusCode } = await getAsync(opts);
 
         if (statusCode === 200) {
-            return _.get(body, 'data');
+            let component = _.get(body, 'data')
+            this._cache.set(compId, component)
+            return component;
         }
 
         if (statusCode === 404) {
