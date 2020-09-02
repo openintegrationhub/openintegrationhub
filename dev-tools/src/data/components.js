35c72cebca007ef74abc87a5602a578114d3831e
heggert <hans@eggert.link>
2020-09-02 14:08:49 +0200

    Add global component and switch default pull policy

--- dev-tools/src/data/components.js
+++ dev-tools/src/data/components.js
@@ -14,4 +14,20 @@ module.exports = [
       },
     ],
   },
+  {
+    distribution: {
+      type: "docker",
+      image: "openintegrationhub/dev-connector:latest",
+    },
+    isGlobal: true,
+    access: "public",
+    name: "Development Component (Private)",
+    description: "A component just for testing",
+    owners: [
+      {
+        id: "t1_admin@local.dev",
+        type: "user",
+      },
+    ],
+  },
 ]
