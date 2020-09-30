23e6f4520a98a21e72431ab6e56a5d1bf4bf57b7
James Weber <weberjm@gmail.com>
2020-09-30 10:50:32 +0200

    Cleanup

--- services/flow-templates/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globalSetup: './test/utils/setup.js',
-  globalTeardown: './test/utils/teardown.js',
-  testEnvironment: './test/utils/mongo-environment.js',
-  testRegex: "./test/test.*.js$",
-};
