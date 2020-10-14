e14e83fe532dbaf47019f557f3508478e3c5e9ad
Ioannis Lafiotis <johnylaf94@gmail.com>
2020-10-14 16:19:57 +0200

    Changed LOGGING_LEVEL to LOG_LEVEL

--- services/app-directory/src/conf/index.js
+++ services/app-directory/src/conf/index.js
@@ -10,7 +10,7 @@ module.exports = {
 
     logging: {
         namespace: optional('LOGGING_NAMESPACE', pjson.name),
-        level: optional('LOGGING_LEVEL', 'error'),
+        level: optional('LOG_LEVEL', 'error'),
     },
     mongoDbConnection: optional('MONGODB_CONNECTION', `mongodb://localhost:27017/${pjson.name}`),
     mongoDbPoolSize: parseInt(process.env.MONGODB_POOL_SIZE, 10) || 5,
