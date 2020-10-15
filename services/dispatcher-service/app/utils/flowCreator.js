9fb9b4623122a3b4425625e1ad40d86cfe129d4d
jkopen <jknoop@wice.de>
2020-10-15 15:10:27 +0200

    Adding functionality to pass flowId to SDF-Adapter flow steps

--- services/dispatcher-service/app/utils/flowCreator.js
+++ services/dispatcher-service/app/utils/flowCreator.js
@@ -160,21 +160,49 @@ async function createFlows(applications, token) {
               'GET',
             );
 
-            const options = {
+            const initialFlow = Object.assign({}, flow);
+            initialFlow.graph = { nodes: [], edges: [] };
+
+            const initialOptions = {
               method: 'POST',
               json: true,
               url: `${flowRepoUrl}/flows`,
+              body: initialFlow,
+              headers: {
+                Authorization: token,
+              },
+            };
+
+            const createResponse = await request(initialOptions);
+
+
+            const flowId = createResponse.body.data.id;
+
+            const nodesLength = flow.graph.nodes.length;
+
+            for (let k = 0; k < nodesLength; k += 1) {
+              if (flow.graph.nodes[k].componentId === sdfAdapterId) {
+                flow.graph.nodes[k].fields.flowId = flowId;
+              }
+            }
+
+            const options = {
+              method: 'PATCH',
+              json: true,
+              url: `${flowRepoUrl}/flows/${flowId}`,
               body: flow,
               headers: {
                 Authorization: token,
               },
             };
+
+
             const response = await request(options);
 
             if (response.statusCode === 201) {
-              app.outbound.flows[j].flowId = response.body.data.id;
+              app.outbound.flows[j].flowId = flowId;
             } else {
-              log.error('Could not create flow:');
+              log.error('Could not create flow:', flowId);
               log.error(response.statusCode);
               log.error(JSON.stringify(response.body));
               return false;
@@ -192,9 +220,35 @@ async function createFlows(applications, token) {
               current,
             );
 
-            const options = {
+            const initialFlow = Object.assign({}, flow);
+            initialFlow.graph = { nodes: [], edges: [] };
+
+            const initialOptions = {
               method: 'POST',
+              json: true,
               url: `${flowRepoUrl}/flows`,
+              body: initialFlow,
+              headers: {
+                Authorization: token,
+              },
+            };
+
+            const createResponse = await request(initialOptions);
+
+
+            const flowId = createResponse.body.data.id;
+
+            const nodesLength = flow.graph.nodes.length;
+
+            for (let l = 0; l < nodesLength; l += 1) {
+              if (flow.graph.nodes[l].componentId === sdfAdapterId) {
+                flow.graph.nodes[l].fields.flowId = flowId;
+              }
+            }
+
+            const options = {
+              method: 'PATCH',
+              url: `${flowRepoUrl}/flows/${flowId}`,
               json: true,
               body: flow,
               headers: {
@@ -204,9 +258,9 @@ async function createFlows(applications, token) {
             const response = await request(options);
 
             if (response.statusCode === 201) {
-              app.inbound.flows[k].flowId = response.body.data.id;
+              app.inbound.flows[k].flowId = flowId;
             } else {
-              log.error('Could not create flow:');
+              log.error('Could not create flow:', flowId);
               log.error(response.statusCode);
               log.error(JSON.stringify(response.body));
               return false;
@@ -269,7 +323,11 @@ async function deleteFlows(applications, token) {
 }
 
 async function updateConfigFlows(original, incoming, token) {
+  console.log('in updateConfigFlows');
   const newConfig = lodash.cloneDeep(incoming);
+
+  console.log('orginal:', original);
+  console.log('incoming:', incoming);
   const oldFlowIds = getFlowIds(original.applications);
   const newFlowIds = getFlowIds(incoming.applications);
 
