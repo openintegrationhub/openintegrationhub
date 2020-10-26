b4d58ba45d9e5056708c7fc13e0b09194b5dc14b
Ioannis Lafiotis <johnylaf94@gmail.com>
2020-10-26 16:12:57 +0100

    Removed the logging-service from the mono-repo

--- services/logging-service/src/errors/api/NotFound.ts
+++
@@ -1,7 +0,0 @@
-import ApiError from './ApiError';
-
-export default class NotFound extends ApiError {
-    public constructor(message: string = 'Not Found') {
-        super(404, message);
-    }
-}
--- services/logging-service/src/spec/test.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Test', () => {
-    it('should work', () => {
-
-    });
-});
