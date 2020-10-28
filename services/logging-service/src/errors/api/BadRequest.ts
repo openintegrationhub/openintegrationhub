8ae31f89ee3ec88aa78bb2b7b12096cb80a1b8a3
Ioannis Lafiotis <46591718+IoannisLafiotis@users.noreply.github.com>
2020-10-28 10:11:03 +0100

    Removed the logging-service from the mono-repo (#1114)
    Co-authored-by: Ioannis Lafiotis <johnylaf94@gmail.com>

--- services/logging-service/src/errors/api/BadRequest.ts
+++
@@ -1,7 +0,0 @@
-import ApiError from './ApiError';
-
-export default class BadRequest extends ApiError {
-    public constructor(message: string = 'BadRequest') {
-        super(400, message);
-    }
-}
