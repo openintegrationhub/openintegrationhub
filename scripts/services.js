f72f9595553b9b7d63d5901c1d20d767b7155e2e
heggert <hans@eggert.link>
2021-01-12 17:46:37 +0100

    Rework scripts to allow more flexible builds

---
+++ scripts/services.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+const basePath = path.resolve(__dirname, "../services")
+const paths = fs.readdirSync(basePath)
+const services = []
+const ignoreServices = ["rds"]
+
+for (const service of paths) {
+  if (ignoreServices.includes(service)) continue
+  const servicePath = path.join(basePath, service)
+  const temp = JSON.parse(fs.readFileSync(`${servicePath}/package.json`))
+  services.push({
+    name: service,
+    version: temp.version,
+    path: servicePath,
+    buildScript: temp.scripts.build
+  })
+}
+
+module.exports = services
