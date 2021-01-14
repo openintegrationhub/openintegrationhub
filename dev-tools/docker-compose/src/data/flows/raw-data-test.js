5080777c87197b33d747d2ad7e0dffc281988bd9
heggert <hans@eggert.link>
2021-01-14 21:25:25 +0100

    Update test setup

--- dev-tools/docker-compose/src/data/flows/raw-data-test.js
+++ dev-tools/docker-compose/src/data/flows/raw-data-test.js
@@ -223,4 +223,68 @@ module.exports = [
       },
     ],
   },
+  {
+    name: 'Webhook8',
+    graph: {
+      nodes: [
+        {
+          id: 'step_1',
+          componentId: 'component5',
+          function: 'testTrigger',
+        },
+        {
+          id: 'step_2',
+          componentId: 'component5',
+          function: 'testAction',
+          nodeSettings: {
+            storeRawRecord: true,
+          },
+        },
+      ],
+      edges: [
+        {
+          source: 'step_1',
+          target: 'step_2',
+        },
+      ],
+    },
+    owners: [
+      {
+        id: 't1_admin@local.dev',
+        type: 'user',
+      },
+    ],
+  },
+  {
+    name: 'Webhook9',
+    graph: {
+      nodes: [
+        {
+          id: 'step_1',
+          componentId: 'component5',
+          function: 'testTrigger',
+        },
+        {
+          id: 'step_2',
+          componentId: 'component5',
+          function: 'testAction',
+          nodeSettings: {
+            storeRawRecord: true,
+          },
+        },
+      ],
+      edges: [
+        {
+          source: 'step_1',
+          target: 'step_2',
+        },
+      ],
+    },
+    owners: [
+      {
+        id: 't1_admin@local.dev',
+        type: 'user',
+      },
+    ],
+  },
 ]
