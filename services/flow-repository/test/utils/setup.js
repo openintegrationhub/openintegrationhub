16032097b2bae0680179bc0559ee854355fefdf5
jkopen <jknoop@wice.de>
2021-01-21 09:23:02 +0100

    Switching to replica set in tests

--- services/flow-repository/test/utils/setup.js
+++ services/flow-repository/test/utils/setup.js
@@ -4,36 +4,37 @@
 const path = require('path');
 const fs = require('fs');
 require('dotenv').config({ path: path.resolve(process.cwd(), '.env.test') });
-const MongodbMemoryServer = require('mongodb-memory-server');
+const { MongoMemoryReplSet } = require('mongodb-memory-server');
 
 const globalConfigPath = path.join(__dirname, 'globalConfig.json');
 
-const mongod = new MongodbMemoryServer.default({
-  instance: {
-    dbName: 'jest',
-  },
-  binary: {
-    version: '4.2.9',
-    ssl: true,
-  },
-  autoStart: false,
+
+const dbName = '_replace_me_';
+const setName = 'jest-set';
+
+const replSet = new MongoMemoryReplSet({
+  autoStart: true,
+  instanceOpts: [
+    { storageEngine: 'wiredTiger' },
+    { storageEngine: 'wiredTiger' },
+  ],
+  replSet: { dbName, name: setName },
 });
 
 module.exports = async () => {
-  if (!mongod.isRunning) {
-    await mongod.start();
-  }
-
-  const mongoConfig = {
-    mongoDBName: 'jest',
-    mongoUri: await mongod.getConnectionString(),
-  };
-
-  // Write global config to disk because all tests run in different contexts.
-  fs.writeFileSync(globalConfigPath, JSON.stringify(mongoConfig));
-  console.log('Config is written');
+  await replSet.waitUntilRunning();
+  const uri = await replSet.getUri();
 
   // Set reference to mongod in order to close the server during teardown.
-  global.__MONGOD__ = mongod;
-  process.env.MONGO_URL = mongoConfig.mongoUri;
+  global.__MONGOD__ = replSet;
+  process.env.MONGO_URL = uri;
+
+  // Write global config to disk because all tests run in different contexts.
+  fs.writeFileSync(
+    globalConfigPath,
+    JSON.stringify({
+      mongoUri: uri,
+    }),
+  );
+  console.log('Config is written');
 };
