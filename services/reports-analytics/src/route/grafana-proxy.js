2261257b5cd6c404bd91386ecb379ea41e5ffabc
jkopen <jknoop@wice.de>
2021-05-20 14:56:54 +0200

    Replacing request

--- services/reports-analytics/src/route/grafana-proxy.js
+++ services/reports-analytics/src/route/grafana-proxy.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const request = require('request');
+const fetch = require('node-fetch');
 const conf = require('../conf');
 
 const router = express.Router();
 
-router.get('/*', (req, res) => {
-    request({
-        uri: `${conf.grafana.url}/${req.originalUrl.replace('/grafana-proxy/', '')}`,
-        method: req.method,
-        headers: {
-            Authorization: `Bearer ${conf.grafana.token}`,
-        },
-    }, () => {
+router.get('/*', async (req, res) => {
+    const options = {
+      method: req.method,
+      headers: {
+        Authorization: `Bearer ${conf.grafana.token}`,
+      },
+    };
 
-    }).pipe(res);
+    await fetch(`${conf.grafana.url}/${req.originalUrl.replace('/grafana-proxy/', '')}`, options)
+      .then(response => response.body.pipe(res));
+
 });
 
 module.exports = router;
