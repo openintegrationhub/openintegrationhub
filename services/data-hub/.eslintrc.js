9cfeb7cc9371c9ad24bf5763104e9ac27076320b
heggert <hans@eggert.link>
2021-07-05 13:15:19 +0200

    Update eslint

--- services/data-hub/.eslintrc.js
+++ services/data-hub/.eslintrc.js
@@ -3,14 +3,13 @@ module.exports = {
   parserOptions: {
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint"],
   extends: [
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
   ],
   rules: {
     "@typescript-eslint/ban-types": 1,
+    "@typescript-eslint/explicit-function-return-type": 0
     // "@typescript-eslint/no-empty-function": 1,
   },
 };
