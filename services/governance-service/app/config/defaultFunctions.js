356fff81081f860a9f520e204edaa1962550ff69
SvenHoeffler <shoeffler@wice.de>
2021-07-09 16:01:27 +0200

    Added prototype of apply policy functionality

--- services/governance-service/app/config/defaultFunctions.js
+++ services/governance-service/app/config/defaultFunctions.js
@@ -1,9 +1,31 @@
+const dotprop = require('dot-prop');
 
 const defaultFunctions = [
   {
     id: false,
     name: 'DefaultFunction1',
     code: 'x * y',
+  },
+
+  {
+    name: 'anonymize',
+    code: (data, duty) => {
+      console.log(data, duty);
+      const returnData = Object.assign({}, data);
+
+      // TODO: Genericise constraint operator handling
+      if (duty.constraint && duty.constraint.operator) {
+        if (duty.constraint.operator === 'applyToKey') {
+          const key = duty.constraint.leftOperand;
+          if (dotprop.has(returnData, key)) dotprop.set(returnData, key, 'XXXXXXXXXX');
+        }
+      }
+
+      return {
+        passes: true,
+        data: returnData,
+      };
+    },
   },
 ];
 
