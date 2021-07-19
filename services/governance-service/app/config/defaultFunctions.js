f237cb3b0b4c84d8046ff1b4f0c41ac421ebd4e4
jkopen <jknoop@wice.de>
2021-07-19 11:22:30 +0200

    Adding frist logic for validating constraints and oparations on nested data elements

--- services/governance-service/app/config/defaultFunctions.js
+++ services/governance-service/app/config/defaultFunctions.js
@@ -1,5 +1,40 @@
 const dotprop = require('dot-prop');
 
+
+function getValuesFromData(data, selector) {
+  const keys = selector.trim().split('.');
+
+  let currentData = data;
+  let found = false;
+
+  // For all keys in selector
+  for (let i = 0; i < keys.length; i += 1) {
+    if (Array.isArray(currentData)) {
+      const results = [];
+      for (let j = 0; j < currentData.length; j += 1) {
+        if (typeof currentData[j] === 'object') {
+          if (keys[i] in currentData[j]) {
+            results.push(currentData[j][keys[i]]);
+            if (i + 1 === keys.length) found = true;
+          }
+        }
+      }
+      currentData = results;
+    } else if (typeof currentData === 'object') {
+      if (keys[i] in currentData) {
+        currentData = currentData[keys[i]];
+        if (i + 1 === keys.length) found = true;
+      }
+    }
+  }
+
+  return {
+    found,
+    value: currentData,
+  };
+}
+
+
 const defaultFunctions = [
   {
     id: false,
@@ -23,6 +58,37 @@ const defaultFunctions = [
 
       return {
         passes: true,
+        data: returnData,
+      };
+    },
+  },
+
+  {
+    name: 'equals',
+    code: (data, permission) => {
+      let passes = false;
+      const returnData = Object.assign({}, data);
+
+      if (permission.constraint && permission.constraint.operator) {
+        if (permission.constraint.operator === 'equals') {
+          const result = getValuesFromData(returnData, permission.constraint.leftOperand);
+          if (result.found) {
+            if (Array.isArray(result.value)) {
+              for (let i = 0; i < result.value.length; i += 1) {
+                if (result.value[i] === permission.constraint.rightOperand) {
+                  passes = true;
+                  break;
+                }
+              }
+            } else if (result.value === permission.constraint.rightOperand) {
+              passes = true;
+            }
+          }
+        }
+      }
+
+      return {
+        passes,
         data: returnData,
       };
     },
