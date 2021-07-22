c2a1671cf3c9f1a008595bcb8dff1c2f3266e181
jkopen <jknoop@wice.de>
2021-07-22 11:46:05 +0200

    Adding logic to handle logic functions or, xone & and in constraints

--- services/governance-service/app/api/controllers/applyPolicy.js
+++ services/governance-service/app/api/controllers/applyPolicy.js
@@ -22,6 +22,91 @@ const router = express.Router();
 
 const log = require('../../config/logger'); // eslint-disable-line
 
+
+function evaluateSingleConstraint(data, currentPermission) {
+  let result = {
+    passes: false,
+  };
+  const handler = defaultFunctions.find(el => el.name === currentPermission.operator);
+  if (handler) {
+    console.log('Has handler');
+    console.log('data', JSON.stringify(data));
+    console.log('currentPermission', currentPermission);
+    result = handler.code(data, { constraint: currentPermission });
+  } else {
+    log.warn(`Attempted to evaluate constraint with operator ${currentPermission.operator} but could not find handler`);
+  }
+
+  console.log(result);
+  return result.passes;
+}
+
+function evaluateConstraints(data, current, logicOperator) {
+  let passes = false;
+
+  console.log('Current:', JSON.stringify(current));
+  if (Array.isArray(current)) {
+    console.log(logicOperator, 'Array');
+    for (let i = 0; i < current.length; i += 1) {
+      console.log('i', i);
+      const result = evaluateConstraints(data, current[i], logicOperator);
+      if (logicOperator) {
+        if (logicOperator === 'or') {
+          console.log('current[i]', current[i]);
+          console.log('logicOperator', logicOperator);
+          console.log('or', result);
+          if (result === true) return true;
+        } else if (logicOperator === 'xone') {
+          console.log('xone', result);
+          if (passes === false && result === true) {
+            passes = true;
+          } else if (passes === true && result === true) {
+            return false;
+          }
+        } else if (logicOperator === 'and') {
+          console.log('and', result);
+          if (result === false) return false;
+          passes = result;
+        }
+      } else {
+        console.log('Implicit and', result);
+        if (result === false) return false;
+        passes = result;
+      }
+    }
+
+    console.log('current', current);
+    console.log('logicOperator', logicOperator);
+    console.log('Result after array loop:', passes);
+    return passes;
+  }
+
+  if (typeof current === 'object') {
+    if ('operator' in current) {
+      // evalute single constraint
+      console.log('evaluate single constraint');
+      console.log(current);
+      return evaluateSingleConstraint(data, current);
+      // return true;
+    } if ('or' in current) {
+      console.log("evaluateConstraints(current.or, 'or')");
+      passes = evaluateConstraints(data, current.or, 'or');
+    } else if ('xone' in current) {
+      console.log("evaluateConstraints(current.xone, 'xone')");
+      passes = evaluateConstraints(data, current.xone, 'xone');
+    } else if ('and' in current) {
+      console.log("evaluateConstraints(current.and, 'and')");
+      passes = evaluateConstraints(data, current.and, 'and');
+    } else {
+      console.log('Logic operator not found in:', current);
+    }
+  } else {
+    console.log('Invalid constraint format:', current);
+  }
+
+  return passes;
+}
+
 // Applies a policy
 router.post('/', jsonParser, async (req, res) => {
   const { data, metadata } = req.body;
@@ -65,6 +150,7 @@ router.post('/', jsonParser, async (req, res) => {
   }
 
   // Repeat the process with permissions and their constraints
+
   if (metadata.policy.permission && metadata.policy.permission.length) {
     for (let i = 0; i < metadata.policy.permission.length; i += 1) {
       const currentPermission = metadata.policy.permission[i];
@@ -74,11 +160,10 @@ router.post('/', jsonParser, async (req, res) => {
         actions = actions.split(',');
       }
       if (actions === false || actions.indexOf(action) > -1) {
-        const handler = defaultFunctions.find(el => el.name === currentPermission.constraint.operator);
-        if (handler) {
-          result = handler.code(result.data, currentPermission);
-        } else {
-          log.warn(`Attempted to apply permission action ${currentPermission.action} but could not find handler`);
+        const passes = evaluateConstraints(result.data, currentPermission.constraint);
+        if (passes === false) {
+          result.passes = false;
+          break;
         }
       }
     }
