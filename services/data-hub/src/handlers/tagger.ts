dd0c163ebe7eeddf69681f88d57d41dd2f691772
SvenHoeffler <shoeffler@wice.de>
2021-07-26 15:03:07 +0200

    Added tagger framework

---
+++ services/data-hub/src/handlers/tagger.ts
@@ -0,0 +1,31 @@
+const { Notation } = require('notation');
+const { comparators } = require('./comparators.ts');
+
+export default function tagObject(object, fields) {
+  const notate = Notation.create;
+  if(!object || !object.content) {
+    console.debug('Content or dataObject not set', object);
+    return object;
+  }
+
+  const { content } = object;
+
+  const tags = [];
+
+  for (let i = 0; i < fields.length; i += 1) {
+    const comparator = comparators[fields[i].comparator];
+    if (!comparator) {
+      console.error('Comparator ' + fields[i].comparator + ' not found.');
+      continue;
+    }
+
+    const result = comparator(content, fields[i].arguments);
+    if (result === true) tags.push(fields[i].tag);
+  }
+
+  const returnObject = Object.assign({}, object);
+  if (!returnObject.meta) returnObject.meta = {};
+  returnObject.meta.tags = tags;
+
+  return returnObject;
+}
--- services/data-hub/src/spec/applyFunctions.spec.ts
+++ services/data-hub/src/spec/applyFunctions.spec.ts
@@ -7,6 +7,7 @@ import DataObject from '../models/data-object';
 import nock from 'nock';
 
 import scoreObject from '../handlers/scorer';
+import tagObject from '../handlers/tagger';
 
 function nockIamIntrospection({
     status = 200,
@@ -59,6 +60,33 @@ const configuration = {
                      "weight":2
                   }
                ]
+            },
+            {
+               "name": "tag",
+               "active": true,
+               "fields":[
+                  {
+                     comparator: 'hasField',
+                     tag: 'Has a Name',
+                     arguments: {
+                       field: 'firstName',
+                     }
+                  }
+               ]
+            },
+            {
+               "name": "tag",
+               "active": true,
+               "fields":[
+                  {
+                     comparator: 'fieldEquals',
+                     tag: 'Is a James',
+                     arguments: {
+                       field: 'firstName',
+                       targetValue: 'James'
+                     }
+                  }
+               ]
             }
          ]
       };
@@ -87,15 +115,11 @@ describe.only('Data Enrichment and Cleansing', () => {
         // refs: [],
         // owners: []
 
-        console.log('Uid:', uid);
-
     });
 
     describe('scorer', () => {
         it('should score test entry correctly', async function () {
             const result = scoreObject(testEntry, configuration.functions[0].fields);
-            console.log('Result:');
-            console.log(JSON.stringify(result));
 
             expect(result.domainId).to.exist;
             expect(result.schemaUri).to.exist;
@@ -104,6 +128,41 @@ describe.only('Data Enrichment and Cleansing', () => {
 
             expect(result.meta.score).to.be.equal(2);
             expect(result.meta.normalizedScore).to.be.equal(1);
+
+            expect(result.content).to.be.an('object');
+
+            expect(result.content.firstName).to.be.equal('James');
+            expect(result.content.lastName).to.be.equal('Blond');
+        });
+    });
+
+    describe('tagger', () => {
+        it('should tag an object correctly with hasField', async function () {
+            const result = tagObject(testEntry, configuration.functions[1].fields);
+
+            expect(result.domainId).to.exist;
+            expect(result.schemaUri).to.exist;
+
+            expect(result.meta).to.be.an('object');
+
+            expect(result.meta.tags).to.deep.equal(['Has a Name']);
+
+            expect(result.content).to.be.an('object');
+
+            expect(result.content.firstName).to.be.equal('James');
+            expect(result.content.lastName).to.be.equal('Blond');
+        });
+
+        it('should tag an object correctly with fieldEquals', async function () {
+            const result = tagObject(testEntry, configuration.functions[2].fields);
+            console.log(testEntry)
+
+            expect(result.domainId).to.exist;
+            expect(result.schemaUri).to.exist;
+
+            expect(result.meta).to.be.an('object');
+
+            expect(result.meta.tags).to.deep.equal(['Is a James']);
 
             expect(result.content).to.be.an('object');
 
