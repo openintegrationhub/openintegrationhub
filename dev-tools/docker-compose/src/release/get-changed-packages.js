673d1f7e1273b2bfb3726848cc6ca8aff979f110
heggert <hans@eggert.link>
2021-08-06 13:00:26 +0200

    Add release helper script

---
+++ dev-tools/docker-compose/src/release/get-changed-packages.js
@@ -0,0 +1,119 @@
+const { execSync } = require('child_process')
+const path = require('path')
+const fs = require('fs')
+const { checkTools } = require('../helper')
+
+function getAvailablePackages(type = 'services') {
+  const basePath = path.resolve(__dirname, `../../../../${type}`)
+  const paths = fs.readdirSync(basePath)
+  const results = []
+
+  for (const pkg of paths) {
+    const pkgPath = path.join(basePath, pkg)
+    const temp = JSON.parse(fs.readFileSync(`${pkgPath}/package.json`))
+    results.push({
+      name: pkg,
+      version: temp.version,
+      dependencies: temp.dependencies,
+      devDependencies: temp.devDependencies,
+      path: pkgPath,
+    })
+  }
+  return results
+}
+
+function getDependentPackages(available = {}, changedLib = '', type = '') {
+  const dependent = []
+  available[type].forEach((availablePkg) => {
+    if (available[type] === 'lib' && availablePkg.name === changedLib) return
+    const match = new RegExp(changedLib, 'i')
+
+    for (const dep of Object.keys(availablePkg.dependencies)) {
+      if (match.test(dep)) {
+        dependent.push(availablePkg.name)
+        return
+      }
+    }
+    for (const devDep of Object.keys(availablePkg.devDependencies)) {
+      if (match.test(devDep)) {
+        dependent.push(availablePkg.name)
+        return
+      }
+    }
+  })
+  return dependent
+}
+
+checkTools(['git'])
+
+async function run() {
+  const tag = process.argv[2]
+
+  const available = {
+    services: getAvailablePackages('services'),
+    lib: getAvailablePackages('lib'),
+  }
+
+  const ignored = {
+    services: ['logic-gateway'],
+    lib: ['secret-service'],
+  }
+
+  const changed = {
+    services: [],
+    lib: [],
+  }
+
+  const dependent = {
+    services: [],
+    lib: [],
+  }
+
+  const total = {
+    services: [],
+    lib: [],
+  }
+
+  if (!tag) throw new Error('Please provide a tag')
+
+  const result = execSync(
+    `git diff tags/${tag} --name-only | grep -E -i -w '^services|^lib' || true`
+  ).toString()
+
+  // create list of services / lib
+  const changes = result.split(/\n/g)
+
+  changes.forEach((fileName) => {
+    const pathSegments = fileName.split('/')
+    if (pathSegments[0] !== 'services' && pathSegments[0] !== 'lib') return
+    if (changed[pathSegments[0]].includes(pathSegments[1])) return
+    // if (ignored[pathSegments[0]].includes(pathSegments[1])) return
+    changed[pathSegments[0]].push(pathSegments[1])
+  })
+
+  // detect services that are dependent on changed libs
+  changed.lib.forEach((changedLib) => {
+    dependent.services = dependent.services.concat(
+      getDependentPackages(available, changedLib, 'services')
+    )
+  })
+
+  total.lib = [...new Set(changed.lib)].filter(
+    (lib) => !ignored.lib.includes(lib)
+  )
+
+  total.services = [
+    ...new Set(changed.services.concat(dependent.services)),
+  ].filter((service) => !ignored.services.includes(service))
+
+  console.log('lib', total.lib.length, total.lib)
+  console.log('services', total.services.length, total.services)
+}
+
+;(async () => {
+  try {
+    await run()
+  } catch (err) {
+    console.log(err)
+  }
+})()
