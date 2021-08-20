89ca3160190e21502f34ff26c9b8ced2cb128948
Ioannis Lafiotis <johnylaf94@gmail.com>
2021-08-20 08:51:37 +0200

    Changed the test

--- services/template-repository/test/test.js
+++ services/template-repository/test/test.js
@@ -259,7 +259,7 @@ describe('Template Validation', () => {
       });
     expect(res.status).toEqual(400);
     expect(res.body.errors).toHaveLength(3);
-    expect(res.body.errors[1].message).toEqual('Cast to ObjectID failed for value "abc" at path "componentId"');
+    expect(res.body.errors[1].message).toEqual('Cast to ObjectId failed for value "abc" at path "componentId"');
     expect(res.body.errors[0].message).toEqual('Cast to ObjectID failed for value "IncorrectSecret" at path "credentials_id"');
     expect(res.body.errors[2].message).toEqual('Flow Templates with more than one node require edges.');
   });
