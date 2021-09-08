5a4953871eff1d2f0c7d2c287ccf639bed446677
Hans <hans@eggert.link>
2021-09-08 16:18:48 +0200

    Add cache folder purge

--- dev-tools/docker-compose/src/packages/purge.js
+++ dev-tools/docker-compose/src/packages/purge.js
@@ -1,7 +1,17 @@
-const cp = require("child_process")
+const { execSync } = require("child_process")
 const { repositoryRoot } = require("../config")
 
-cp.execSync(
+execSync(
   `cd ${repositoryRoot} && sudo find . -name "node_modules" -type d -prune -exec rm -rf '{}' +`,
+  { stdio: "inherit" }
+)
+
+execSync(
+  `cd ${repositoryRoot} && sudo find . -name ".yarn_cache" -type d -prune -exec rm -rf '{}' +`,
+  { stdio: "inherit" }
+)
+
+execSync(
+  `cd ${repositoryRoot} && sudo find . -name ".npm_cache" -type d -prune -exec rm -rf '{}' +`,
   { stdio: "inherit" }
 )
