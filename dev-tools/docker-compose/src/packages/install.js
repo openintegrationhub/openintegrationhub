0ded32f167aab3cf91f090270898be4bfb5ed3ce
Hans <hans@eggert.link>
2021-09-15 18:14:41 +0200

    Use plain 16

--- dev-tools/docker-compose/src/packages/install.js
+++ dev-tools/docker-compose/src/packages/install.js
@@ -1,9 +1,9 @@
-const path = require("path")
+const path = require('path')
 const cp = require('child_process')
 
 const repositoryRoot = path.resolve(__dirname, '../../../../')
 const nodeImage = 'node:16-bullseye'
 cp.execSync(
-  `docker run --rm -it -e npm_config_cache=/usr/src/app/.npm_cache -v ${repositoryRoot}:/usr/src/app ${nodeImage} sh -ci 'cd /usr/src/app; npm i --package-lock-only --verbose'`,
+  `docker run --rm -it -e npm_config_cache=/usr/src/app/.npm_cache -v ${repositoryRoot}:/usr/src/app ${nodeImage} sh -ci 'cd /usr/src/app; npm i'`,
   { stdio: 'inherit' }
 )
