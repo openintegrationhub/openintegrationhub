dffd519517584030491e5294bc9b08742c191983
heggert <hans@eggert.link>
2021-09-16 10:41:13 +0200

    Use npx to start nodemon

--- dev-tools/test-component/start.sh
+++ dev-tools/test-component/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./node_modules/.bin/nodemon -L ../../lib/ferryman/runGlobal.js --watch ../../lib/ferryman --watch ./
+npx nodemon -L ../../lib/ferryman/runGlobal.js --watch ../../lib/ferryman --watch ./
