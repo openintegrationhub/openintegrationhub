ba1fd83de3a7df156d2b0ddce22137c6b8d817f7
heggert <hans@eggert.link>
2021-09-16 21:37:35 +0200

    Fix linting

--- dev-tools/docker-compose/src/packages/purge.js
+++ dev-tools/docker-compose/src/packages/purge.js
@@ -1,18 +1,19 @@
-const { execSync } = require("child_process")
-const path = require("path")
+const { execSync } = require('child_process')
+const path = require('path')
+
 const repositoryRoot = path.resolve(__dirname, '../../../../')
 
 execSync(
   `cd ${repositoryRoot} && sudo find . -name "node_modules" -type d -prune -exec rm -rf '{}' +`,
-  { stdio: "inherit" }
+  { stdio: 'inherit' }
 )
 
 execSync(
   `cd ${repositoryRoot} && sudo find . -name ".yarn_cache" -type d -prune -exec rm -rf '{}' +`,
-  { stdio: "inherit" }
+  { stdio: 'inherit' }
 )
 
 execSync(
   `cd ${repositoryRoot} && sudo find . -name ".npm_cache" -type d -prune -exec rm -rf '{}' +`,
-  { stdio: "inherit" }
+  { stdio: 'inherit' }
 )
