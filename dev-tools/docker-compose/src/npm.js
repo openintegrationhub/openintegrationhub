d23c13df152e33d06866b36091811aacc1cb6e47
heggert <hans@eggert.link>
2021-10-01 12:28:53 +0200

    Add auto pull docker image and refactor code

--- dev-tools/docker-compose/src/npm.js
+++ dev-tools/docker-compose/src/npm.js
@@ -7,6 +7,14 @@ const nodeImage = 'node:16-bullseye'
 const args = process.argv[2] ? process.argv.splice(2).join(' ') : ''
 
 execSync(
-  `docker run --rm --name npmRunner -it -e npm_config_cache=/usr/src/app/.npm_cache -v ${repositoryRoot}:/usr/src/app ${nodeImage} sh -ci 'cd /usr/src/app; npm ${args}'`,
+  `docker pull ${nodeImage} && \
+  docker run \
+  --rm \
+  --name npmRunner \
+  -it \
+  -e npm_config_cache=/usr/src/app/.npm_cache \
+  -v ${repositoryRoot}:/usr/src/app \
+  ${nodeImage} \
+  sh -ci 'cd /usr/src/app; npm ${args}'`,
   { stdio: 'inherit' }
 )
