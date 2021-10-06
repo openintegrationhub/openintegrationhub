e788ea9acfd039084432374dbeb565e8bfababf2
heggert <hans@eggert.link>
2021-10-06 11:34:29 +0200

    Fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -12,7 +12,7 @@ services.forEach((service) => {
     } catch(err) {
         // build new version
         console.log('Build new docker image')
-        if (service.buildScript) execSync(`cd ${service.path} && npm build`, { stdio: [0, 1, 2] })
+        if (service.buildScript) execSync(`cd ${service.path} && npm run build`, { stdio: [0, 1, 2] })
         execSync('echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin', { stdio: [0, 1, 2] });
         execSync(`cd ${service.path} && npm run build:docker`, { stdio: [0, 1, 2], env: Object.assign({VERSION: service.version}, process.env) });
         execSync(`docker tag openintegrationhub/${service.name}:${service.version} openintegrationhub/${service.name}:latest`, { stdio: [0, 1, 2] });
