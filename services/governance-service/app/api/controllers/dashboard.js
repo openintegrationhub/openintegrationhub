8eb1f0cd4c08f51447e079f6da897a9d0477dd09
SvenHoeffler <shoeffler@wice.de>
2021-12-06 14:27:26 +0100

    Added first iteration of dashboard logic

--- services/governance-service/app/api/controllers/dashboard.js
+++ services/governance-service/app/api/controllers/dashboard.js
@@ -19,30 +19,56 @@ const jsonParser = bodyParser.json();
 const router = express.Router();
 
 const log = require('../../config/logger'); // eslint-disable-line
+const { getObjectDistribution, checkFlows, getRefs } = require('../../utils/dashboard');
+const { getProvenanceEvents } = require('./mongo');
 
 // Gets overview of data distribution
 router.get('/distribution', jsonParser, can('tenant.all'), async (req, res) => {
+  const distribution = await getObjectDistribution(req.user.sub);
 
+  if (!distribution) return res.status(404).send({ error: [{ message: 'Could not gather distribution' }] });
+
+  res.status(200).send(distribution);
 });
 
 // Gets detailed information about a single object
 router.get('/objectStatus/:id', jsonParser, can('tenant.all'), async (req, res) => {
+  let referenceData;
+  if (req.query.recordId) {
+    referenceData = await getRefs(false, req.params.id, req.headers.authorization);
+  } else {
+    referenceData = await getRefs(req.params.id, false, req.headers.authorization);
+  }
 
+  const id = req.query.recordId ? referenceData.id : req.params.id;
+
+  if (!id) return res.status(400).send({ errors: [{ message: 'Could not find object oihUid' }] });
+
+  const events = await getProvenanceEvents(req.user, 100, 1, { 'entity.id': id });
+
+  res.status(200).send({ events, refs: referenceData ? referenceData.refs : false });
 });
 
 // Gets a list of current warnings and advisories
 router.get('/warnings', jsonParser, can('tenant.all'), async (req, res) => {
+  const flowWarnings = await checkFlows(req.headers.authorization);
 
+  // TODO: Check for failed delete warnings
+
+  res.status(200).send({ flowWarnings });
 });
 
 // Gets a combined list of all available data
 router.get('/dashboard', jsonParser, can('tenant.all'), async (req, res) => {
+  const distribution = await getObjectDistribution(req.user.sub);
+  const flowWarnings = await checkFlows(req.headers.authorization);
 
+  res.status(200).send({ distribution, flowWarnings });
 });
 
 // Mark a particular warning as resolved or ignored
-router.patch('/warnings/:id', jsonParser, can('tenant.all'), async (req, res) => {
-
-});
+// router.patch('/warnings/:id', jsonParser, can('tenant.all'), async (req, res) => {
+//
+// });
 
 module.exports = router;
--- services/governance-service/app/utils/dashboard.js
+++ services/governance-service/app/utils/dashboard.js
@@ -1,16 +1,105 @@
+/* eslint no-continue: "off" */
+
+const fetch = require('node-fetch');
 const config = require('../config/index');
 const log = require('../config/logger');
+const { getProvenanceEvents } = require('../api/controllers/mongo');
 
 // Get all refs of a given object
-async function getRefs(id, recordUid) {
+async function getRefs(id, recordUid, token) {
+  if (!id && !recordUid) return false;
 
+  const url = id ? `${config.dataHubUrl}/${id}` : `${config.dataHubUrl}/recordId/${id}`;
+
+  const response = await fetch(
+    url,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: token,
+      },
+    },
+  );
+
+  if (response.status !== 200) return false;
+
+  const object = response.json();
+  if (!object || !object.refs) return false;
+
+  return {
+    refs: object.refs,
+    id: object.id,
+  };
+}
+
+// Calculates object distribution by refs
+async function getObjectDistribution(user) {
+  try {
+    const allEvents = await getProvenanceEvents(user);
+
+    const serviceCounts = {};
+
+    for (let i = 0; i < allEvents.length; i += 1) {
+      const currentEvent = allEvents[i];
+      const serviceEntry = currentEvent.actedOnBehalfOf.find((el) => el.agentType === 'Application');
+      if (!serviceEntry) continue;
+
+      const serviceName = serviceEntry.actedOnbehalfOf;
+
+      if (!(serviceName in serviceCounts)) {
+        serviceCounts[serviceName] = {
+          received: 0,
+          updated: 0,
+          created: 0,
+          deleted: 0,
+        };
+      }
+
+      switch (currentEvent.activity.activityType) {
+      case 'ObjectReceived':
+        serviceCounts[serviceName].received += 1;
+        break;
+      case 'ObjectUpdated':
+        serviceCounts[serviceName].updated += 1;
+        break;
+      case 'ObjectCreated':
+        serviceCounts[serviceName].created += 1;
+        break;
+      case 'ObjectDeleted':
+        serviceCounts[serviceName].deleted += 1;
+        break;
+      default:
+        break;
+      }
+    }
+
+    return serviceCounts;
+  } catch (e) {
+    log.error(e);
+    return false;
+  }
 }
 
 // Get flows of a user and check if any cause a warning
-async function checkFlows(token) {
+async function getFlows(token) {
+  const response = await fetch(
+    config.flowRepoUrl,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: token,
+      },
+    },
+  );
 
+  if (response.status !== 200) return false;
+
+  const flows = response.json();
+  return flows;
 }
 
 module.exports = {
-
+  getRefs,
+  getFlows,
+  getObjectDistribution,
 };
