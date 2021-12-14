67b1ff6657c049cb0a1b69a2d240c068e0a3afea
jkopen <jknoop@wice.de>
2021-12-14 16:54:18 +0100

    Further improvements

--- services/governance-service/app/public/graph.js
+++ services/governance-service/app/public/graph.js
@@ -1,35 +1,33 @@
 window.initGraph = function (id, elements) {
   window.graph = cytoscape({
     container: document.getElementById(id),
-    elements,
-    style: [
-      {
-        selector: 'node',
-        css: {
-          // shape: 'round-rectangle',
-          shape: 'ellipse',
-          width: 100,
-          height: 100,
-          selectionColor: '#ffffff',
-          selectionGlowColor: '#ffffff',
-          hoverGlowColor: '#ffffff',
-          // 'background-color':'#61bffc',
-          'background-color': '#ffffff',
-          'background-image': 'data(image)',
-          // 'background-fit': 'cover',
-          // 'background-width': 50,
-          // 'background-height': 10,
-          // 'background-position-x': 5,
-          content: 'data(id)',
-          'text-valign': 'center',
-          'text-halign': 'center',
-          'border-width': 1,
-          'border-opacity': 0.0,
-          // 'border-width': 4,
-          // 'border-style': 'solid',
-          // 'border-color': '#51afec',
-          // 'border-opacity': 0.9,
+    elements: elements,
+      style: [
+        {
+          selector: 'node',
+          css: {
+            // shape: 'round-rectangle',
+            shape: 'ellipse',
+            width: 100,
+            height: 100,
+            //'background-color':'#61bffc',
+            'background-color':'#ffffff',
+            'background-image': 'data(image)',
+            //'background-fit': 'cover',
+            //'background-width': 50,
+            //'background-height': 10,
+            //'background-position-x': 5,
+            content: 'data(id)',
+            'text-valign': 'center',
+            'text-halign': 'center',
+            'border-width': 1,
+            'border-opacity': 0.0,
+            //'border-width': 4,
+            //'border-style': 'solid',
+            //'border-color': '#51afec',
+            //'border-opacity': 0.9,
 
+          },
         },
       },
       {
@@ -110,26 +108,28 @@ window.initGraph = function (id, elements) {
     const nodeFlows = event.target.data('nodeFlows');
 
     overlay.innerHTML = `
-      <div><span>Received:</span> <b>${event.target.data("received")}</b></div>
-      <div><span>Sent:</span> <b>${event.target.data("sent")}</b></div>
+      <div><h3>Connector</h3></div>
+      <div><span>Out:</span> <b>${event.target.data("received")}</b></div>
 
+      <div><span>In</span></div>
       <div><span>Created:</span> <b>${event.target.data("created")}</b></div>
       <div><span>Updated:</span> <b>${event.target.data("updated")}</b></div>
-      <div><span>Deleted:</span> <b>${event.target.data("deleted")}</b></div>
+      <div><span>Deleted:</span> <b class="deleted">${event.target.data("deleted")}</b></div>
 
       <div><h3>Flows</h3></div>
       <div><span>In:</span> <b>${nodeFlows.flowsIn}</b></div>
       <div><span>Out:</span> <b>${nodeFlows.flowsOut}</b></div>
-
-      <div class="flowList"><span>⇤</span> <b>${nodeFlows.in.join(', ')}</b></div>
-      <div class="flowList"><span>⇥</span> <b>${nodeFlows.out.join(', ')}</b></div>
+      <div class="flowList"><span><img src="/static/icons/move_to_inbox_black_24dp.svg"></span> <b>${nodeFlows.in.join(', ')}</b></div>
+      <div class="flowList"><span><img src="/static/icons/outbox_black_24dp.svg"></span> <b>${nodeFlows.out.join(', ')}</b></div>
     `;
 
-    // overlay
-    let h = event.renderedPosition.y;
-    let w = event.renderedPosition.x;
-    const height = 340;
-    const width = 140;
+    //⇤⇥
+
+    //overlay
+    var h = event.renderedPosition.y
+    var w = event.renderedPosition.x;
+    var height = document.getElementById('overlay').offsetHeight;
+    var width = document.getElementById('overlay').offsetWidth;
 
     const hDiff = (h + height) - window.innerHeight;
     const wDiff = (w + width) - window.innerWidth;
