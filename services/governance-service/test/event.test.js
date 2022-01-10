9a13b238ef7b8c494a6520831bd763248d4c0a99
jkopen <jknoop@wice.de>
2022-01-10 15:46:38 +0100

    Prevent race condition in Tests

--- services/governance-service/test/event.test.js
+++ services/governance-service/test/event.test.js
@@ -40,6 +40,7 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
+  if (mongoose.connection && mongoose.connection.db) await mongoose.connection.db.dropDatabase();
   mongoose.connection.close();
   app.close();
 });
