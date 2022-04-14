35df1e89ede6f9387276898bc555956d01b41e7f
Robb Winkle <robb@blendededge.com>
2022-04-14 10:38:04 -0400

    Removed HOST_OIH_DIRECTORY changes to minikube setup

--- dev-tools/minikube/setup.sh
+++ dev-tools/minikube/setup.sh
@@ -848,10 +848,10 @@ if [ "$os" == "Darwin" ]; then
     if [ "$machine" == "ARM" ]; then
         minikube start --driver=docker --memory $MK_MEMORY --cpus $MK_CPUS --mount=true --mount-string="${HOST_OIH_DIRECTORY}:/openintegrationhub"
     else 
-        minikube start --driver=hyperkit --vm=true --memory $MK_MEMORY --cpus $MK_CPUS  --mount=true --mount-string="${HOST_OIH_DIRECTORY}:/openintegrationhub"
+        minikube start --driver=hyperkit --vm=true --memory $MK_MEMORY --cpus $MK_CPUS
     fi
 else
-    minikube start --memory $MK_MEMORY --cpus $MK_CPUS --mount=true --mount-string="${HOST_OIH_DIRECTORY}:/openintegrationhub"
+    minikube start --memory $MK_MEMORY --cpus $MK_CPUS
 fi
 
 ###
