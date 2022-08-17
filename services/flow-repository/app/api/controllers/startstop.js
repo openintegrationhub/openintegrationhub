135193599374b238e3b536f99064becc0f5169ec
jkopen <jknoop@wice.de>
2022-08-17 14:25:41 +0200

    Adding endpoints for start/stop of multiple flows

--- services/flow-repository/app/api/controllers/startstop.js
+++ services/flow-repository/app/api/controllers/startstop.js
@@ -19,6 +19,104 @@ const router = express.Router();
 
 const log = require('../../config/logger'); // eslint-disable-line
 
+// Start multiple flows
+router.post('/multiple/start', jsonParser, can(config.flowControlPermission), async (req, res) => {
+  const errors = false;
+  const results = [];
+  const flowIds = req.body;
+  try {
+    if (!Array.isArray(flowIds)) {
+      return res.status(500).send({ errors: [{ message: 'No flow ids given. Expecting an array of flow ids', code: 500 }] });
+    }
+
+    for (let i = 0; i < flowIds.length; i += 1) {
+      const flowId = flowIds[i];
+
+      if (!mongoose.Types.ObjectId.isValid(flowId)) {
+        results.push({ errors: [{ message: 'Invalid id', flowId, code: 400 }] });
+        continue; // eslint-disable-line no-continue
+      }
+
+      const currentFlow = await storage.getFlowById(flowId, req.user);
+      if (!currentFlow) {
+        results.push({ errors: [{ message: 'No flow with this ID found', flowId, code: 404 }] });
+      }
+
+      const flow = await storage.startingFlow(req.user, flowId);
+
+      const ev = {
+        headers: {
+          name: 'flow.starting',
+        },
+        payload: flow,
+      };
+
+      ev.payload.startedBy = req.user.sub;
+
+      await publishQueue(ev);
+
+      results.push({ data: { id: flow.id, status: flow.status }, meta: {} });
+    }
+
+    if (errors) {
+      return res.status(500).send(results);
+    }
+    return res.status(200).send(results);
+  } catch (e) {
+    log.error(e);
+    results.push(e);
+    return res.status(500).send(results);
+  }
+});
+
+// Stop multiple flows
+router.post('/multiple/stop', jsonParser, can(config.flowControlPermission), async (req, res) => {
+  const errors = false;
+  const results = [];
+  const flowIds = req.body;
+  try {
+    if (!Array.isArray(flowIds)) {
+      return res.status(500).send({ errors: [{ message: 'No flow ids given. Expecting an array of flow ids', code: 500 }] });
+    }
+
+    for (let i = 0; i < flowIds.length; i += 1) {
+      const flowId = flowIds[i];
+
+      if (!mongoose.Types.ObjectId.isValid(flowId)) {
+        results.push({ errors: [{ message: 'Invalid id', flowId, code: 400 }] });
+        continue; // eslint-disable-line no-continue
+      }
+
+      const currentFlow = await storage.getFlowById(flowId, req.user);
+      if (!currentFlow) {
+        results.push({ errors: [{ message: 'No flow with this ID found', flowId, code: 404 }] });
+      }
+
+      const flow = await storage.stoppingFlow(req.user, flowId);
+
+      const ev = {
+        headers: {
+          name: 'flow.stopping',
+        },
+        payload: flow,
+      };
+
+      await publishQueue(ev);
+
+      results.push({ data: { id: flow.id, status: flow.status }, meta: {} });
+    }
+
+    if (errors) {
+      return res.status(500).send(results);
+    }
+    return res.status(200).send(results);
+  } catch (e) {
+    log.error(e);
+    results.push(e);
+    return res.status(500).send(results);
+  }
+});
+
 // Start a flow
 router.post('/:id/start', jsonParser, can(config.flowControlPermission), async (req, res) => {
   const flowId = req.params.id;
