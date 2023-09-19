6b3099642199e6a8a34caa1fb497674593ba3050
Robb Winkle <robb@blendededge.com>
2023-09-19 09:55:05 -0400

    Include source information in payload from webhooks and scheduler (#1490)
    * Add externalExecId as request ID in webhook service
    
    * Added scheduler event metadata and data payload
    Added source metdata to webhook payload
    
    * Rename source id to name to avoid confusion
    Move externalExecId inside of source for easier propogation through a flow
    
    * Fixed unit tests for new metadata payload

--- services/scheduler/src/SchedulePublisher.js
+++ services/scheduler/src/SchedulePublisher.js
@@ -1,5 +1,6 @@
 const { SchedulePublisher } = require('@openintegrationhub/scheduler');
 const { Event } = require('@openintegrationhub/event-bus');
+const cronParser = require('cron-parser');
 
 class OIH_SchedulePublisher extends SchedulePublisher {
   constructor({ eventBus }) {
@@ -7,6 +8,8 @@ class OIH_SchedulePublisher extends SchedulePublisher {
     this._eventBus = eventBus;
   }
   async scheduleFlow(flow) {
+    const interval = cronParser.parseExpression(flow.cron);
+    const nextSheduledTimestamp = interval.next();
     const event = new Event({
       headers: {
         name: 'flow.execute',
@@ -14,10 +17,32 @@ class OIH_SchedulePublisher extends SchedulePublisher {
       },
       payload: {
         flow,
+        msg: {
+          data: {
+            sheduledTimestamp: new Date().toISOString(),
+            nextSheduledTimestamp,
+          },
+          metadata: {
+            source: {
+              name: 'scheduler',
+              type: 'scheduler',
+              externalExecId: generateRequestId(),
+            },
+          },
+        },
       },
     });
     this._eventBus.publish(event);
   }
+}
+
+function generateRequestId() {
+  // NOTE the result should be in the same format as provided by proxy in from of this server (nginx)
+  const numbers = [];
+  for (let i = 0; i < 32; i++) {
+      numbers[i] = Math.floor(Math.random() * 16).toString(16);
+  }
+  return numbers.join('');
 }
 
 module.exports = OIH_SchedulePublisher;
