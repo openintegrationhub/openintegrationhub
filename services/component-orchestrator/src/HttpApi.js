6292769da53f72bbe52c1778e404c4a5a16c8268
Pavel Nedelko <pnedelko@gmail.com>
2023-10-27 19:39:00 +0300

    get execution by ID API (#1501)

--- services/component-orchestrator/src/HttpApi.js
+++ services/component-orchestrator/src/HttpApi.js
@@ -1,7 +1,4 @@
 const express = require('express');
-// const Lib = require('backend-commons-lib');
-// const { errors } = Lib;
-const auth = require('basic-auth');
 
 class HttpApi {
     /**
@@ -9,17 +6,35 @@ class HttpApi {
      * @param opts.config
      * @param opts.logger
      */
-    constructor({ config, logger }) {
+    constructor({ config, logger, flowStateDao }) {
         this._config = config;
         this._logger = logger.child({ service: 'HttpApi' });
-        // this._componentOrchestrator = componentOrchestrator
-        // this._flowsDao = flowsDao;
-        // this._secretsDao = secretsDao;
-        // this._snapshotsDao = snapshotsDao;
         this._app = express();
-        // this._app.get('/v1/tasks/:flowId/steps/:stepId', this._setIamToken.bind(this), this._getStepInfo.bind(this));
-        // this._app.get('/v1/components/:componentId/start', this._setIamToken.bind(this), this._getStepInfo.bind(this));
-        // this._app.get('/v1/components/:componentId/stop', this._setIamToken.bind(this), this._getStepInfo.bind(this));
+
+        this._app.get('/executions', async (req, res, next) => {
+            try {
+                res.json([]);
+            } catch (e) {
+                return next(e);
+            }
+        });
+
+        this._app.get('/executions/:execId', async (req, res, next) => {
+            try {
+                const execution = await flowStateDao.findById(req.params.execId);
+
+                if (!execution) {
+                    return res.status(404).json({
+                        error: 'Not found'
+                    });
+                }
+
+                res.json(execution);
+            } catch (e) {
+                return next(e);
+            }
+        });
+
         this._app.get('/healthcheck', this._healthcheck.bind(this));
         this._app.use(this._errorHandler.bind(this));
     }
@@ -43,76 +58,6 @@ class HttpApi {
             error: err.message || 'Unknown error'
         });
     }
-
-    _setIamToken(req, res, next) {
-        // Ferryman passes an IAM token as a password part of the Basic auth header
-        const user = auth(req);
-        if (!user) {
-            return next(new Error('Failed to parse basic auth'));
-        }
-        req.iamToken = user.pass;
-        return next();
-    }
-
-    // /**
-    //  * This API is required by flow nodes in order to get a node's configuration.
-    //  * @param req
-    //  * @param res
-    //  * @returns {Promise<void>}
-    //  * @private
-    //  */
-    // async _getStepInfo(req, res, next) {
-    //     const { flowId, stepId } = req.params;
-    //     const logger = this._logger.child({ flowId, stepId });
-
-    //     try {
-    //         const flow = await this._flowsDao.findById(flowId);
-    //         if (!flow) {
-    //             throw new errors.ResourceNotFoundError('Flow is not found');
-    //         }
-    //         const node = await flow.getNodeById(stepId);
-    //         if (!node) {
-    //             throw new errors.ResourceNotFoundError('Node is not found');
-    //         }
-
-    //         const nodeConfig = node.fields || {};
-    //         if (node.credentials_id) {
-    //             logger.trace({ secretId: node.credentials_id }, 'About to get secret by ID');
-    //             const secret = await this._secretsDao.findById(node.credentials_id, {
-    //                 auth: {
-    //                     token: req.iamToken
-    //                 }
-    //             });
-    //             if (!secret) {
-    //                 throw new errors.ResourceNotFoundError(`Secret ${node.credentials_id} is not found`);
-    //             }
-    //             Object.assign(nodeConfig, secret.value);
-    //             logger.trace({ nodeConfig }, 'Got secret. Injected into the node config.');
-    //         }
-
-    //         let snapshot = {};
-
-    //         try {
-    //             snapshot = await this._snapshotsDao.findOne({
-    //                 flowId,
-    //                 stepId,
-    //                 auth: { token: req.iamToken }
-    //             });
-    //         } catch (e) {
-    //             logger.warn(e, 'Failed to get snapshot');
-    //         }
-
-    //         res.status(200);
-    //         res.json({
-    //             id: node.id,
-    //             function: node.function,
-    //             config: nodeConfig,
-    //             snapshot
-    //         });
-    //     } catch (e) {
-    //         return next(e);
-    //     }
-    // }
 
     /**
      * Healthcheck endpoint.
