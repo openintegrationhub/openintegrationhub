18e189a70289ec92f44848cf492ad88be182a576
Beatriz Martin <beatriz.martin@new-work.se>
2023-11-10 10:37:12 +0100

    feat: add adapter to the secret service

--- services/secret-service/src/adapter/preprocessor/zoom/index.js
+++ services/secret-service/src/adapter/preprocessor/zoom/index.js
@@ -1,19 +1,16 @@
 const rp = require('request-promise');
 
-module.exports = async ({
-    secret,
-    tokenResponse,
-}) => {
+module.exports = async ({ secret, tokenResponse }) => {
+    const authToken = `Bearer ${tokenResponse.access_token}`;
     const resp = await rp.get({
         uri: 'https://api.zoom.us/v2/users/me',
         headers: {
-            Authorization: tokenResponse.access_token,
+            Authorization: authToken,
         },
         json: true,
     });
 
     secret.value.externalId = resp.id;
-    secret.value.externalAccountId = resp.account_id;
 
     return secret;
 };
