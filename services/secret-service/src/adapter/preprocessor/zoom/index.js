341d32179784d7d816c6c97d05497b2aad6da9ec
Beatriz Martín de Juan <beatriz.martin@new-work.se>
2023-11-21 14:30:57 +0100

    fix: zoom preprocessor to use non deprecated library (#1511)
    * fix: zoom preprocessor to use non deprecated library
    
    * chore: revert package changes
    
    This reverts commit 39118b7023016799653848a877e84e61732640a3.
    
    * fix: zoom preprocessor to use non deprecated library
    
    * chore: package-lock updated
    
    * chore: bump version zoom preprocessor fix

--- services/secret-service/src/adapter/preprocessor/zoom/index.js
+++ services/secret-service/src/adapter/preprocessor/zoom/index.js
@@ -1,14 +1,20 @@
-const rp = require('request-promise');
+const fetch = require('node-fetch');
 
 module.exports = async ({ secret, tokenResponse }) => {
     const authToken = `Bearer ${tokenResponse.access_token}`;
-    const resp = await rp.get({
-        uri: 'https://api.zoom.us/v2/users/me',
+
+    const response = await fetch('https://api.zoom.us/v2/users/me', {
+        method: 'GET',
         headers: {
             Authorization: authToken,
         },
-        json: true,
     });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch user data: ${response.statusText}`);
+    }
+
+    const resp = await response.json();
 
     secret.value.externalId = resp.id;
 
